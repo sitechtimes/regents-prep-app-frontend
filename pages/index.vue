fd87525bc2531f9e70ab3a43e495d062640ca3e6
AbigailLabanok <113201330+AbigailLabanok@users.noreply.github.com>
2023-10-16 11:01:21 -0400

    main page css

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <h1>Regents Prep App</h1>
+  <div class="bg-[#f1f0d6] h-screen flex flex-col items-center justify-center ">
+    <h1 class="text-9xl font-medium drop-shadow-xl">Regents Prep App</h1>
     <label>
       <button class="button" id="loginRedirect">
         <NuxtLink to="/login" class="loginLink">Login</NuxtLink>
--- pages/login.vue
+++ pages/login.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Login</h1>
+    <h1 class="text-3xl font-bold underline">Login</h1>
   </div>
 </template>
 
