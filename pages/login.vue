f2c18a03f25d2d3d83cd50fee89b0364e40cba39
GhTmp1A <stanbutter4@gmail.com>
2023-10-16 11:01:45 -0400

    Setup for login page

--- pages/login.vue
+++ pages/login.vue
@@ -1,6 +1,28 @@
 <template>
   <div>
+    <div></div>
     <h1>Login</h1>
+    <h2>Email</h2>
+    <input
+      type="input"
+      name="email"
+      id="emailInput"
+      class="relative mt-2 rounded-md shadow-sm border-opacity-4"
+    />
+    <!-- Note that the tailwind for both inputs is a placeholder just to see the input boxes. Please feel free to change them if needed. -->
+    <h2>Password</h2>
+    <input
+      type="input"
+      name="password"
+      id="passwordInput"
+      class="relative mt-2 rounded-md shadow-sm border-opacity-4"
+    />
+    <h3>Forgot Password?</h3>
+    <button>
+      <NuxtLink to="/teacher/teacherdashboard" class="loginLink"
+        >Login</NuxtLink
+      >
+    </button>
   </div>
 </template>
 
--- pages/teacher/teacherdashboard.vue
+++ pages/teacher/teacherdashboard.vue
@@ -1,5 +1,7 @@
 <template>
-  <div></div>
+  <div>
+    <h1>teacher dashboard</h1>
+  </div>
 </template>
 
 <script></script>
