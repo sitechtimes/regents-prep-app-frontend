2c5c3d51eb86bdd15734cfb06bc72aad7bc1e72d
AbigailLabanok <113201330+AbigailLabanok@users.noreply.github.com>
2023-10-19 11:01:35 -0400

    css for login page - in progress

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class=" h-screen flex flex-col items-center justify-center gap-[57px]">
+  <div class="h-screen flex flex-col items-center justify-center gap-[57px]">
     <h1 class="w-[2000px] h-[167px] text-center text-black text-[150px] font-medium drop-shadow-xl">Regents Prep App</h1>
     <label>
       <button id="loginRedirect" class="button bg-[#426B1F] w-[265px] h-24 pt-[19.72px] rounded-[20px] shadow-inner flex-col items-center gap-[447.72px] inline-flex">
--- pages/login.vue
+++ pages/login.vue
@@ -1,41 +1,38 @@
 <template>
-  <div>
-14-login-page-setup
-    <div></div>
-    <h1>Login</h1>
-    <h2>Email</h2>
-    <input
+  <div
+    class="w-[779px] h-[690px] flex flex-col items-center justify-center bg-lime-800 rounded-[40px] border-2 border-black"
+  >
+    <h1 class="w-[293px] h-[90px] text-center text-[#FDFDF0] text-[90px] font-medium">Login</h1>
+    <h2 class="w-[222px] h-[151px] text-[#F2F0CC] text-[50px] font-normal">Email:</h2>
+    <input 
       type="text"
       name="email"
       id="emailInput"
-      class="relative mt-2 rounded-md shadow-sm border-opacity-4"
+      class="relative mt-2 shadow-sm border-opacity-4 w-[703px] h-[65px] bg-[#FAF9E5] border-[#797979]"
       v-model="email"
     />
     <!-- Note that the tailwind for both inputs is a placeholder just to see the input boxes. Please feel free to change them if needed. -->
-    <h2>Password</h2>
+    <h2 class="w-[222px] h-[151px] text-[#F2F0CC] text-[50px] font-normal">Password:</h2>
     <input
       type="input"
       name="password"
       id="passwordInput"
-      class="relative mt-2 rounded-md shadow-sm border-opacity-4"
+      class="relative mt-2 shadow-sm border-opacity-4 w-[703px] h-[65px] bg-[#FAF9E5] border-[#797979]"
     />
-    <h3>Forgot Password?</h3>
-    <button @click.prevent="getUser">
+    <h3 class="w-[251px] h-[47px] text-[#F2F0CC] text-3xl font-medium">Forgot Password?</h3>
+    <button @click.prevent="getUser" class="bg-[#AAB840] w-[202px] h-[81px] pt-[19.72px] rounded-[20px] shadow-inner flex-col items-center inline-flex">
       <!--  <NuxtLink to="/teacher/teacherdashboard" class="loginLink"
         > -->Login<!-- </NuxtLink
       > -->
     </button>
-
   </div>
 </template>
 
-
-
 <script>
-import {userState} from   '~/stores/users'
+import { userState } from "~/stores/users";
 
 export default {
-  name: 'Login',
+  name: "Login",
   data() {
     return {
       email: ref(""),
@@ -44,10 +41,10 @@ export default {
   },
   methods: {
     async getUser() {
-      const userInfo = userState() // With Nuxt, userInfo has to be declared through the userState() every time it is used in a function.
-      console.log(userInfo.user.loggedIn)
+      const userInfo = userState(); // With Nuxt, userInfo has to be declared through the userState() every time it is used in a function.
+      console.log(userInfo.user.loggedIn);
       console.log(this.email);
-      this.email = ''
+      this.email = "";
     },
   },
 };
