ab7d7c2a9ec8c9d8b559b2cf6c1e387fef7e2480
AbigailLabanok <113201330+AbigailLabanok@users.noreply.github.com>
2023-10-23 11:00:54 -0400

    login styling and button hover

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,26 @@
 <template>
   <div class="h-screen flex flex-col items-center justify-center gap-[57px]">
-    <h1 class="w-[2000px] h-[167px] text-center text-black text-[150px] font-medium drop-shadow-xl">Regents Prep App</h1>
+    <h1
+      class="w-[2000px] h-[167px] text-center text-black text-[150px] font-medium drop-shadow-xl"
+    >
+      Regents Prep App
+    </h1>
     <label>
-      <button id="loginRedirect" class="button bg-[#426B1F] w-[265px] h-24 pt-[19.72px] rounded-[20px] shadow-inner flex-col items-center gap-[447.72px] inline-flex">
-        <NuxtLink to="/login" class="loginLink w-[294px] h-[50px] text-center text-white text-[40px] font-medium tracking-wide">Login</NuxtLink>
+      <button
+        id="loginRedirect"
+        class="button bg-[#426B1F] w-[265px] h-24 pt-[19.72px] rounded-[20px] shadow-inner flex-col items-center gap-[447.72px] inline-flex hover:scale-105 duration-300"
+      >
+        <NuxtLink
+          to="/login"
+          class="loginLink w-[294px] h-[50px] text-center text-white text-[37px] font-medium tracking-wide"
+          >Login</NuxtLink
+        >
       </button>
     </label>
-  </div>  
+  </div>
 </template>
 
 <script></script>
 
-<style lang="css" scoped></style>
+<style lang="css" scoped>
+</style>
--- pages/login.vue
+++ pages/login.vue
@@ -1,38 +1,49 @@
 <template>
-  <div class="w-[779px] h-[690px] flex flex-col items-center justify-center bg-lime-800 rounded-[40px] border-2 border-black m-auto">
-    <h1 class="w-[293px] h-[90px] text-center text-[#FDFDF0] text-[90px] font-medium">
-      Login
-    </h1>
-    <h2 class="w-[222px] h-[151px] text-[#F2F0CC] text-[50px] font-semibold pt-[60px] drop-shadow-md pr-[700px]">
-      Email:
-    </h2>
-    <input
-      type="text"
-      name="email"
-      id="emailInput"
-      class="relative shadow-sm border-opacity-4 w-[703px] h-[65px] bg-[#FAF9E5] border-[#797979]"
-      v-model="email"
-    />
-    <!-- Note that the tailwind for both inputs is a placeholder just to see the input boxes. Please feel free to change them if needed. -->
-    <h2 class="w-[222px] h-[151px] text-[#F2F0CC] text-[50px] font-semibold pt-[60px] pr-[700px] drop-shadow-md">
-      Password:
-    </h2>
-    <input
-      type="input"
-      name="password"
-      id="passwordInput"
-      class="relative mt-2 shadow-sm border-opacity-4 w-[703px] h-[65px] bg-[#FAF9E5] border-[#797979]"
-    />
-    <h3 class="w-[280px] h-[47px] text-[#F2F0CC] text-3xl font-bold pr-[700px]">
-      Forgot Password?
-    </h3>
-    <button
-      @click.prevent="getUser"
-      class="bg-[#AAB840] w-[202px] h-[81px] rounded-[20px] shadow-inner items-center justify-center inline-flex">
-      <NuxtLink to="/teacher/teacherdashboard" class="loginLink text-[40px] font-medium text-[#F8F8F8] pb-[5px]">
+  <div class="h-screen flex items-center justify-center">
+    <div
+      class="w-[779px] h-[690px] flex flex-col items-center justify-center bg-lime-800 rounded-[40px] border-2 border-black m-auto"
+    >
+      <h1
+        class="w-[293px] h-[90px] text-center text-[#FDFDF0] text-[90px] font-medium"
+      >
         Login
-      </NuxtLink> 
-    </button>
+      </h1>
+      <h2
+        class="w-[222px] h-[151px] text-[#F2F0CC] text-[50px] font-semibold pt-[50px] drop-shadow-md pr-[700px]"
+      >
+        Email:
+      </h2>
+      <input
+        type="text"
+        name="email"
+        id="emailInput"
+        class="relative shadow-sm border-opacity-4 w-[703px] h-[65px] bg-[#FAF9E5] border-[#797979] text-3xl px-2"
+        v-model="email"
+      />
+      <!-- Note that the tailwind for both inputs is a placeholder just to see the input boxes. Please feel free to change them if needed. -->
+      <h2
+        class="w-[222px] h-[151px] text-[#F2F0CC] text-[50px] font-semibold pt-[60px] pr-[700px] drop-shadow-md"
+      >
+        Password:
+      </h2>
+      <input
+        type="input"
+        name="password"
+        id="passwordInput"
+        class="relative mt-2 shadow-sm border-opacity-4 w-[703px] h-[65px] bg-[#FAF9E5] border-[#797979] text-3xl px-2"
+      />
+      <button
+        @click.prevent="getUser"
+        class="bg-[#AAB840] w-[202px] h-[81px] rounded-[20px] shadow-inner items-center justify-center inline-flex mt-[27px] hover:scale-105 duration-300"
+      >
+        <NuxtLink
+          to="/teacher/teacherdashboard"
+          class="loginLink text-[40px] font-medium text-[#F8F8F8] pb-[5px]"
+        >
+          Login
+        </NuxtLink>
+      </button>
+    </div>
   </div>
 </template>
 
