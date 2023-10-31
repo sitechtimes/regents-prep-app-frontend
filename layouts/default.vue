0212beb819600f1548abb6d9838a2308922ac558
AbigailLabanok <113201330+AbigailLabanok@users.noreply.github.com>
2023-10-31 11:00:00 -0400

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,8 @@
 <template>
   <div>
-    <header>
-      <nav class="h-[109px] bg-[#EAE9CA] shadow">
-        <h1>Navbar</h1>
-      </nav>
-    </header>
-      <slot />
+    <nav class="h-[109px] bg-[#EAE9CA] shadow">
+      <h1>Navbar</h1>
+    </nav>
+    <slot />
   </div>
 </template>
