c2caf7ca7f37276942b675cb4fea1f4e7022e4e8
AbigailLabanok <113201330+AbigailLabanok@users.noreply.github.com>
2023-11-02 11:01:03 -0400

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,17 +2,29 @@
   <div>
     <nav class="h-[109px] bg-[#EAE9CA] shadow">
       <div class="flex flex-row">
-        <h1>aslidfhasdf</h1>
-        <h2>afglasdf</h2>
+        <div class="w-[355px] h-[70px]">
+          <div
+            class="w-[390px] h-[83px] bg-[#F2F0CC] rounded-2xl shadow-[inset_0_4px_7px_rgba(0,0,0,0.3)] drop-shadow-md"
+          >
+            <h2
+              class="w-[390px] h-[70px] text-center justify-center text-[#4E684A] text-[55px] font-semibold font-['Outfit']"
+            >
+              Teacher Name
+            </h2>
+          </div>
+        </div>
+        <div class="w-[277px] h-[73px]">
+          <div
+            class="w-[277px] h-[73px] bg-[#426B1F] rounded-[20px] shadow shadow-inner shadow-[inset_0_4px_7px_rgba(0,0,0,0.3)] drop-shadow-md"
+          >
+            <h1
+              class="w-[277px] h-[73px] text-center text-white text-[50px] font-medium font-['Outfit']"
+            >
+              Dashboard
+            </h1>
+          </div>
+        </div>
       </div>
-      <!-- <div class="w-[355px] h-[70px] left-[30px] top-[15px] absolute">
-        <div class="w-[390px] h-[83px] left-[-11px] top-[-2px] absolute bg-[#F2F0CC] rounded-2xl shadow-[inset_0_4px_7px_rgba(0,0,0,0.3)]"></div>
-        <h2 class="w-[363px] h-[70px] absolute text-center text-[#4E684A] text-[55px] font-semibold font-['Outfit']">Teacher Name</h2>
-    </div>
-    <div class="w-[277px] h-[73px] left-[1000px] relative">
-        <div class="w-[277px] h-[73px] left-0 top-0 absolute bg-[#426B1F] rounded-[20px] shadow shadow-inner"></div>
-        <div class="w-[252px] h-[73px] left-[12px] top-0 absolute text-center text-white text-[50px] font-medium font-['Outfit']">Dashboard</div>
-    </div> -->
     </nav>
     <slot />
   </div>
