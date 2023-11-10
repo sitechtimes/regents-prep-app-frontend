9b1430cd0a37364afdb468da9323952bdd138126
GhTmp1A <stanbutter4@gmail.com>
2023-11-10 11:02:20 -0500

    Began on the class page on the teacher

---
+++ pages/user-[teacher]/class-[id].vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+// request from api is made
+//All of the students for this particular class are recieved.
+</script>
+
+<template>
+  <!--Navigation bar will go here.-->
+  <div id="students">
+    <div>
+      <h2>Students</h2>
+      <SubmitButton> View Class Statistics</SubmitButton>
+    </div>
+    <div id="student-container">
+      <div id="student">
+        <!--v-for="student in students" goes into div-->
+        <!-- <h2> {{ student.lastName }}, {{ student.firstName }} </h2>-->
+      </div>
+    </div>
+    <div id="assignClassHomework"></div>
+  </div>
+</template>
+
+<style lang="css" scoped></style>
