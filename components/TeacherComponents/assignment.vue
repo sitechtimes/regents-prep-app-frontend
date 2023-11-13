4cb7aa56f41522573a861c0285680c1852330892
GhTmp1A <stanbutter4@gmail.com>
2023-11-13 10:58:27 -0500

    Further progress is being made.
    Currently, the component for the individual assignments displayed on the teacherdashboard is being made. This is a placeholder component.
    
    Note: The file structure for the student and teacher folders has to be redone with 'index'.

---
+++ components/TeacherComponents/assignment.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h2 class="bg-black"><slot></slot></h2>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style></style>
--- pages/user-[teacher]/class-[id].vue
+++ pages/user-[teacher]/class-[id].vue
@@ -5,18 +5,44 @@
 
 <template>
   <!--Navigation bar will go here.-->
-  <div id="students">
-    <div>
-      <h2>Students</h2>
-      <SubmitButton> View Class Statistics</SubmitButton>
-    </div>
-    <div id="student-container">
-      <div id="student">
-        <!--v-for="student in students" goes into div-->
-        <!-- <h2> {{ student.lastName }}, {{ student.firstName }} </h2>-->
+  <div id="mainPage">
+    <div id="students" class="m-1">
+      <div>
+        <h2>Students</h2>
+        <SubmitButton> View Class Statistics</SubmitButton>
+      </div>
+      <div id="student-container">
+        <div id="student">
+          <!--v-for="student in students" goes into div-->
+          <!-- <h2> {{ student.lastName }}, {{ student.firstName }} </h2>-->
+        </div>
+      </div>
+      <div id="assignClassHomework">
+        <SubmitButton>Assign Class Homework</SubmitButton>
       </div>
     </div>
-    <div id="assignClassHomework"></div>
+    <div id="studentDashboard">
+      <div id="studentDashboardHeader">
+        <h3>Student Dashboard</h3>
+      </div>
+      <div id="studentNavigation">
+        <!--<h2>{{ student.lastname }}, {{ student.firstName }}</h2>-->
+        <SubmitButton>View Statistics</SubmitButton>
+      </div>
+      <div id="currentAssigments">
+        <div id="currentAssignmentsHeader">
+          <h3>Current Assignments</h3>
+        </div>
+        <!--Components with assignments in the forms of divs will go here.-->
+      </div>
+      <div id="pastAssigments">
+        <div id="pastAssignmentsHeader">
+          <h3>Past Assignments</h3>
+        </div>
+        <TeacherComponentsassignment>test</TeacherComponentsassignment>
+        <!--Components with assignments in the forms of divs will go here.-->
+      </div>
+    </div>
   </div>
 </template>
 
