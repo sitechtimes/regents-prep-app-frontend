b49029ea70a3c322d21fb0b6f725ab956ab88f78
GhTmp1A <stanbutter4@gmail.com>
2023-11-16 11:02:26 -0500

    Fixing width and margin for component

--- components/TeacherComponents/assignment.vue
+++ components/TeacherComponents/assignment.vue
@@ -2,23 +2,29 @@
 const props = defineProps({
   name: { type: String },
   month: { type: Number },
-  day: { type: Number},
+  day: { type: Number },
 });
 </script>
 
 <template>
-  <div 
-    class="w-[549px] h-[57px] flex flex-col items-center justify-center bg-[#AAB941] bg-opacity-62 rounded-full relative shadow-blue shadow-sm"
+  <div
+    class="max-w-min m-2.5 h-[57px] flex flex-col items-center justify-center bg-[#AAB941] bg-opacity-62 rounded-full relative shadow-2xl"
   >
-  <div class="static" >
-    <h2 id="assignmentName" class="place-content-center text-black" >
-      {{ name }}
-    </h2>
-  </div>
-      <div class="flex flex-col items-center justify-center absolute right-0 rounded-[73.50px] 
-      backdrop-blur-sm bg-[#8B9930]  h-[57px] w-[107px] " >
-        <h3 id="dueDate" class="text-center text-white">Due: {{ month }}/{{ day }}</h3>
-      </div>
+    <div class="static w-max mx-16">
+      <h2
+        id="assignmentName"
+        class="text-[36px] place-content-center text-black"
+      >
+        {{ name }}
+      </h2>
+    </div>
+    <div
+      class="flex flex-col items-center justify-center absolute right-0 rounded-[73.50px] shadow-black bg-[#8B9930] h-[57px] w-[107px]"
+    >
+      <h3 id="dueDate" class="text-[21px] text-center text-white">
+        Due: {{ month }}/{{ day }}
+      </h3>
+    </div>
   </div>
 </template>
 
