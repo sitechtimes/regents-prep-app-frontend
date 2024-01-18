8048b9603f95d040bc6b2939cd0293d717272e1d
GhTmp1A <stanbutter4@gmail.com>
2024-01-18 10:37:29 -0500

    Setting up the teacherdashboard
    The teacherdashboard has been set up for stylization, and all the other pages have been re-stylized to align with the figma.

--- pages/user-[teacher]/classpeople-[id].vue
+++ pages/user-[teacher]/classpeople-[id].vue
@@ -8,46 +8,20 @@
   <div id="mainPage">
     <div id="students" class="m-1">
       <div>
-        <h2>Students</h2>
-        <SubmitButton> View Class Statistics</SubmitButton>
+        <input />
+        <h2>Search for student</h2>
       </div>
       <div id="student-container">
         <div id="student">
           <!--v-for="student in students" goes into div-->
           <!-- <h2> {{ student.lastName }}, {{ student.firstName }} </h2>-->
+          <h2>For every student in students</h2>
         </div>
-      </div>
-      <div id="assignClassHomework">
-        <SubmitButton>Assign Class Homework</SubmitButton>
       </div>
     </div>
-    <div id="studentDashboard">
-      <div id="studentDashboardHeader">
-        <h3>Student Dashboard</h3>
-      </div>
-      <div id="studentNavigation">
-        <!--<h2>{{ student.lastname }}, {{ student.firstName }}</h2>-->
-        <SubmitButton>View Statistics</SubmitButton>
-      </div>
-      <div id="currentAssigments">
-        <div id="currentAssignmentsHeader">
-          <h3>Current Assignments</h3>
-        </div>
-        <!--<TeacherComponentsAssignment />
-        TeacherComponentsAssignment with v-for for the 5 current assignments for the current student will go here.
-        -->
-      </div>
-      <div id="pastAssigments">
-        <div id="pastAssignmentsHeader">
-          <h3>Past Assignments</h3>
-        </div>
-        <TeacherComponentsAssignment name="Solar Power" />
-        <!--TeacherComponentsAssignment with v-for for the 5 past assignments for the current student will go here.
-        -->
-      </div>
-      <div>
-        <submitButton>Assign Student Homework</submitButton>
-      </div>
+    <div id="view.assign">
+      <SubmitButton>View Class Statistics</SubmitButton>
+      <SubmitButton>Assign Class Homework</SubmitButton>
     </div>
   </div>
 </template>
