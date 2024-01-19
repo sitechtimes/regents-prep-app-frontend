8b3b6c70892e2901344878c17f0dea84f64526db
GhTmp1A <stanbutter4@gmail.com>
2024-01-19 11:02:37 -0500

    Fixing state in all files

--- stores/users.ts
+++ stores/users.ts
@@ -1,13 +1,17 @@
 import { defineStore } from 'pinia'
 
 export const userState = defineStore("settings", () => {
-    const user = ref({
+/*     const user = ref({
         email: '',
         username: '',
         fullname:'', //Note: full name is going to be requested from the api using the username.
         student: false,
-    });
+    }); */
+    const email = ref("");
+    const username = ref("");
+    const fullname = ref(""); //Note: full name is going to be requested from the api using the username.
+    const student = ref(false);
     const loggedIn = ref(false);
-    return {user, loggedIn}
+    return {email, username, fullname, student, loggedIn}
     
 });
