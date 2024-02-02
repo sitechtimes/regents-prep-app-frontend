863dcac65344fc5e93e7b535bbc898711acc3106
Darren Chin <darrenschoolemail@gmail.com>
2024-02-02 10:55:55 -0500

    user_Type

--- stores/users.ts
+++ stores/users.ts
@@ -4,13 +4,13 @@ export const userState = defineStore("settings", () => {
   const email = ref("");
   const username = ref("");
   const fullname = ref("");
-  const student = ref(false);
+  const user_type = ref("");
   const loggedIn = ref(false);
   return {
     email,
     username,
     fullname,
-    student,
+    user_type,
     loggedIn,
   };
 });
