ef9159354d0d77c100447f68e5890ba5ff9ed2bf
GhTmp1A <stanbutter4@gmail.com>
2024-02-26 11:02:04 -0500

    Questions.ts from options to composition

--- stores/questions.ts
+++ stores/questions.ts
@@ -4,14 +4,20 @@ import {
   questionStateInterface,
 } from "~/interfaces/interfaces";
 
-export const useQuestions = defineStore("questions", {
-  state: (): questionStateInterface => {
+export const useQuestions = defineStore("questions", () => {
+
+  const assignmentName = ref<string>("");
+  const qText = ref<string>("");
+  const timeLeft = ref<number>(0);
+  const qLeft = ref<number>(0);
+  const answers = [] as Array<String>
+  
     return {
-      assignmentName: "",
-      qText: "",
-      timeLeft: 0,
-      qLeft: 0,
-      answers: [],
+      assignmentName,
+      qText,
+      timeLeft,
+      qLeft,
+      answers,
     };
 
     //The necessary properties are returned, and the state is in the questionStateInterface, as typescript Pinia is utilized.
@@ -22,5 +28,5 @@ export const useQuestions = defineStore("questions", {
       //this.question = data.answer
       //This code (and likely the entire file) will be changed when the api is developed.
     }
-  },
-});
+  })
+
