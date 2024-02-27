5261d5ab4b0edccfc870efe32cc668c16e0291fa
GhTmp1A <stanbutter4@gmail.com>
2024-02-26 19:13:25 -0500

    Fixed typescript problem
    Ask between difference in Ref<string> and Ref<String>

--- stores/questions.ts
+++ stores/questions.ts
@@ -5,28 +5,26 @@ import {
 } from "~/interfaces/interfaces";
 
 export const useQuestions = defineStore("questions", () => {
+  const assignmentName: Ref<String> = ref("");
+  const qText: Ref<String> = ref("");
+  const timeLeft: Ref<Number> = ref(0);
+  const qLeft: Ref<Number> = ref(0);
+  const answers: Ref<Array<String>> = ref([]);
 
-  const assignmentName = ref<string>("");
-  const qText = ref<string>("");
-  const timeLeft = ref<number>(0);
-  const qLeft = ref<number>(0);
-  const answers = [] as Array<String>
-  
-    return {
-      assignmentName,
-      qText,
-      timeLeft,
-      qLeft,
-      answers,
-    };
+  return {
+    assignmentName,
+    qText,
+    timeLeft,
+    qLeft,
+    answers,
+  };
 
-    //The necessary properties are returned, and the state is in the questionStateInterface, as typescript Pinia is utilized.
+  //The necessary properties are returned, and the state is in the questionStateInterface, as typescript Pinia is utilized.
 
-    async function fill() {
-      //const {data} = await api.from('api').select()
-      //this.question = data.question
-      //this.question = data.answer
-      //This code (and likely the entire file) will be changed when the api is developed.
-    }
-  })
-
+  async function fill() {
+    //const {data} = await api.from('api').select()
+    //this.question = data.question
+    //this.question = data.answer
+    //This code (and likely the entire file) will be changed when the api is developed.
+  }
+});
