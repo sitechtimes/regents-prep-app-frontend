75aa1dd7762238cb439f8ab7777f233484f642e0
AbigailLabanok <113201330+AbigailLabanok@users.noreply.github.com>
2024-03-05 11:05:42 -0500

    fixed slider

--- components/StudentComponents/AssignmentsTemp.vue
+++ components/StudentComponents/AssignmentsTemp.vue
@@ -17,18 +17,18 @@ filter.forEach((e) => {
 </script>
 
 <template>
-  <div
+  <button
     v-if="dateValue"
-    class="w-max h-[75px] mb-[2rem] flex flex-row relative bg-primary bg-opacity-60 rounded-full shadow-black shadow-innervar hover:shadow-none"
+    class="w-max h-[75px] mb-[2rem] flex flex-row relative bg-primary bg-opacity-60 rounded-full shadow-black shadow-innervar hover:shadow-none after-bg-secondary"
   >
     <div class="text-[50px] px-[2rem] font-medium">{{ name }}</div>
 
     <div
-      class="w-[75px] h-full right-[0%] bg-primary rounded-full shadow-black shadow-innervar"
+      class="w-[75px] h-full right-[0%] bg-primary rounded-full shadow-black shadow-innervar shadow-inner"
     >
       <div class="w-full h-full text-center text-white text-[50px] font-medium">
         {{ questions }}
       </div>
     </div>
-  </div>
+  </button>
 </template>
--- pages/user-[student]/class-[classCode]/index.vue
+++ pages/user-[student]/class-[classCode]/index.vue
@@ -41,7 +41,7 @@ const pastArr: any[] = [];
     <div
       class="h-[38px] flex flex-row justify-between mb-[1rem] text-[26px] font-medium"
     >
-      <div class="h-[60px] w-[600px] text-[35px] ml-[150px] mt-[15px] flex items-center bg-yellow-50 rounded-[27px]">
+      <div class="h-[60px] w-[470px] text-[35px] ml-[80px] mt-[15px] flex items-center bg-bg-light rounded-[27px]">
         <label class="switch relative inline-block h-full aspect-[1.75]">
           <input
             class="opacity-0 w-0 h-0"
@@ -50,22 +50,22 @@ const pastArr: any[] = [];
             checked
           />
           <span
-            class="slider round absolute before:absolute cursor-pointer inset-0 rounded-[34px] before:rounded-[50%] duration-[0.4s] before:duration-[0.4s] bg-[#ccc] before:bg-white before:content[''] before:h-full before:aspect-square"
+            class="slider round border-black border-[1px] absolute right-4 before:absolute cursor-pointer inset-0 rounded-[34px] before:rounded-[50%] duration-[0.4s] before:duration-[0.4s] bg-[#ccc] before:bg-bg-dark before:border-[1px] before:border-black before:content[''] before:h-full before:aspect-square"
           ></span>
         </label>
 
-        <div class="w-[80%] right-[0%] pl-4">{{ toggle }} Assignments</div>
+        <div class="w-[80%] right-[0%] text-center m-auto">{{ toggle }} Assignments</div>
       </div>
 
       <button
         v-if="CurrentStatus"
-        class="h-[60px] w-[350px] text-[35px] mr-[150px] mt-[15px] text-center text-white bg-secondary rounded-[27px] shadow-innervar shadow-black justify-center items-center hover:scale-105 hover:drop-shadow-2xl duration-300 hover:shadow-transparent"
+        class="h-[60px] w-[370px] text-[35px] mr-[100px] mt-[15px] text-center text-white bg-secondary rounded-[27px] shadow-innervar shadow-black justify-center items-center hover:scale-105 hover:drop-shadow-2xl duration-300 hover:shadow-transparent"
       >
         Start Assignment
       </button>
       <button
         v-if="PastStatus"
-        class="h-[60px] w-[350px] text-[35px] mr-[150px] mt-[15px] text-center text-white bg-secondary rounded-[27px]"
+        class="h-[60px] w-[370px] text-[35px] mr-[100px] mt-[15px] text-center text-white bg-secondary rounded-[27px]"
       >
         View Statistics
       </button>
