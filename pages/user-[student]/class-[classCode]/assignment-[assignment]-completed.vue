e55fb2a192eff5ca9721e39a5ad829bc500b4b1e
Ryan Chen <113121132+RyanChen979@users.noreply.github.com>
2024-04-03 10:33:21 -0400

    editing css

--- pages/user-[student]/class-[classCode]/assignment-[assignment]-completed.vue
+++ pages/user-[student]/class-[classCode]/assignment-[assignment]-completed.vue
@@ -9,20 +9,20 @@ definePageMeta({
 
 <template>
   <div
-    class="flex flex-col h-[50rem] max-w-[80%] mx-auto items-center justify-evenly text-center text-4xl"
+    class="flex flex-col h-[50rem] max-w-[80%] mx-auto items-center justify-evenly text-center text-4xl font-semibold"
   >
-    <h3
-      class="border-solid border-[1px] border-black rounded-[25px] p-[5rem] bg-[#f9f8e3] font-bold"
-    >
+    <h3 class="rounded-[25px] p-[5rem] bg-[#f9f8e3] font-bold">
       Your score for this assignment is shown below
     </h3>
 
-    <div class="rounded-[50px] p-[2rem] bg-[#aab941] text-white font-extrabold">
-      5/5
+    <div
+      class="rounded-[50px] p-[2rem] bg-[#aab941] text-white text-[50px] font-bold"
+    >
+      5 / 5
     </div>
 
     <button
-      class="rounded-[50px] p-[2rem] bg-[#5c8335] shadow-black shadow-innertop text-white font-extrabold"
+      class="rounded-[50px] p-[2rem] bg-[#5c8335] shadow-black shadow-innertop text-white font-bold hover:scale-105 hover:drop-shadow-2xl duration-300 hover:shadow-transparent"
     >
       View Statistics
     </button>
