5903ae84cc3502e2f166b6c237d0cb68189f0065
GhTmp1A <stanbutter4@gmail.com>
2024-09-10 20:33:31 -0400

    Variable into link integration
    All api request links have been replaced, however placing a variable for the actual url does not work at the moment.

--- stores/users.ts
+++ stores/users.ts
@@ -27,7 +27,7 @@ export const userState = defineStore("state", () => {
         fullUserName = email.split("@")[0];
       }
       const response = await fetch(
-        `http://127.0.0.1:8000`,
+        `http://127.0.0.1:8000/api/token/`,
         {
           method: "POST",
           headers: {
