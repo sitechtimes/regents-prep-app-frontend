23672778dcd0b49e1cf00c98b1f8fb5b5f8de061
AbigailLabanok <abigail.labanok@gmail.com>
2024-09-17 10:58:28 -0400

    Revert "Update index.vue"
    This reverts commit 8e5540496669c02fe0b2715491c6b3b39608154d.

--- pages/user-[student]/class-[classCode]/index.vue
+++ pages/user-[student]/class-[classCode]/index.vue
@@ -4,6 +4,7 @@ import { userState } from "~/stores/users";
 import { useQuestions } from "~/stores/questions";
 import { userClass } from "~/stores/class";
 import { studentAssignments } from "~/interfaces/interfaces";
+
 const router = useRouter();
 const userStore = userState();
 const userQuestions = useQuestions();
