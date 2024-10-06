16ab18c8c8874e8e50bcfdf7f46f1b5defa9b648
GhTmp1A <stanbutter4@gmail.com>
2024-10-06 12:20:41 -0400

    Removed placeholder value.

--- stores/StudentStore/questions.ts
+++ stores/StudentStore/questions.ts
@@ -27,7 +27,7 @@ export const useQuestions = defineStore("questions", () => {
 
 
   const router = useRouter();
-  const attempts_remaining = ref<number>(2);
+  const attempts_remaining = ref<number>(0);
 
   function $reset() {//This function resets every property within the question store if a student decides to navigate to the student dashboard while completing the assignment.
     classCode.value = "";
