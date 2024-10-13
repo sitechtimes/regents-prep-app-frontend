ddab5fd2bfcbb9767029ce9e0b25d1f13744e042
DubNubz <iamanasianboi12457@gmail.com>
2024-10-12 23:47:52 -0400

    add teacher routes

---
+++ pages/teacher/course/[courseCode]/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>teacher course view</h1>
+    <p>{{ route.params.courseCode }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute();
+</script>
+
+<style scoped></style>
