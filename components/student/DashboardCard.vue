82c9343e157e307579d43c9e6767daf25c438e33
lawrence <91681777+whydoesthecodenotwork@users.noreply.github.com>
2024-10-14 23:23:45 -0400

    pills here!

--- components/student/DashboardCard.vue
+++ components/student/DashboardCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="card hover:shadow-lg cursor-pointer border-2 border-[var(--faded-bg-color)] rounded-lg overflow-hidden w-[45rem] flex flex-col items-center justify-center transition-all duration-300 ease-in-out"
+    class="card hover:shadow-lg hover:dark:shadow-[var(--gray)] cursor-pointer border-2 border-[var(--faded-bg-color)] rounded-lg overflow-hidden w-[45rem] flex flex-col items-center justify-center transition-all duration-300 ease-in-out"
   >
     <div class="w-full h-24 flex flex-col items-start justify-end p-2" :style="{ backgroundColor: subjectColors[course.subject] }">
       <h2 :title="course.name" class="text-2xl font-semibold text-nowrap overflow-ellipsis w-full overflow-hidden">{{ course.name }}</h2>
@@ -15,11 +15,18 @@
             .filter((a) => !a.submitted)
             .sort((a, b) => a.due.getTime() - b.due.getTime())
             .slice(0, 3)"
+          :key="assignment.id"
         >
           <p class="font-medium" :title="assignment.due.toLocaleString()">Due {{ formatDate(assignment.due, currentTime) }}</p>
           <NuxtLink class="assignment" :to="`/student/course/${course.id}/${assignment.id}`" @click="$event.stopPropagation()">
+            <div class="relative flex items-center rounded-full max-w-2 min-h-4 h-full bg-[var(--gray)] overflow-hidden hover:max-w-16">
+              <span
+                class="absolute size-full"
+                :style="`transform: translateY(${(1 - assignment.questionsCompleted / assignment.questionsLength) * 100}%); background-color: ${subjectColors[course.subject]}`"
+              ></span>
+              <span class="w-full z-10 text-center font-mono px-2"> {{ assignment.questionsCompleted }}/{{ assignment.questionsLength }} </span>
+            </div>
             <span>{{ assignment.name }}</span>
-            <span v-if="assignment.type === 'student'"> ({{ assignment.questionsCompleted }}/{{ assignment.questionsLength }})</span>
           </NuxtLink>
         </div>
       </div>
@@ -40,10 +47,17 @@ const currentTime = ref(new Date());
 </script>
 
 <style scoped>
+.assignment {
+  display: grid;
+  gap: 0.8ch;
+  grid-template-columns: min-content auto;
+  padding: 2px 0;
+}
+
 @media (hover: hover) and (pointer: fine) {
   .card:hover {
     --tw-border-opacity: 1;
-    border-color: rgb(229 231 235 / var(--tw-border-opacity)) /* #e5e7eb */;
+    border-color: calc(var(--text-color) / var(--tw-border-opacity)) /* #e5e7eb */;
   }
 
   .assignment:hover {
