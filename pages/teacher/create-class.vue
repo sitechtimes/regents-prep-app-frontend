cabaabd5c880557078da0382ab5411334c54e0a4
AbigailLabanok <abigail.labanok@gmail.com>
2024-10-17 10:58:56 -0400

    working on creating create class page

---
+++ pages/teacher/create-class.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex items-center justify-center flex-col w-screen min-h-screen bg-gray py-12">
+    <div class="flex items-center justify-center flex-col bg-[color:var(--bg-color)] p-4 rounded-3xl mb-4">
+      <h1>Create New Class</h1>
+      <div class="relative flex items-start justify-center flex-col gap-1">
+        <label for="class-name">Class Name:</label>
+        <input id="class-name" type="class-name" required v-model="className" />
+      </div>
+      <div>
+        <label for="">Choose Course Type:</label>
+        <select name="" id=""></select>
+      </div>
+      <button>Create</button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const className = ref("");
+</script>
+
+<style scoped></style>
