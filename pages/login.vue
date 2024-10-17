50ceab1f2996a8928b9095383645950e2e1c2174
lawrence <91681777+whydoesthecodenotwork@users.noreply.github.com>
2024-10-17 00:19:56 -0400

    bogdan's battle against css

--- pages/login.vue
+++ pages/login.vue
@@ -1,52 +1,67 @@
 <template>
   <div class="flex items-center justify-center flex-col w-screen min-h-screen bg-gray py-12">
-    <a href="/"><img class="logo h-32 transition duration-500 animate-spin"
+    <a href="/"
+      ><img
+        class="logo h-32 transition duration-500 animate-spin"
         src="https://substackcdn.com/image/fetch/f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2F88d26018-fa1a-4b92-a8b9-d8ed3f9e178e_3840x2160.png"
-        aria-hidden="true" /></a>
+        aria-hidden="true"
+    /></a>
     <h1 class="text-5xl font-bold mb-8">Welcome{{ showLogin ? " back" : "" }}!</h1>
 
     <div class="flex items-center justify-center flex-col bg-[color:var(--bg-color)] p-4 rounded-3xl mb-4">
       <h3 class="mb-4" v-show="showLogin">Log in to your not Vent Defeater account</h3>
       <h3 class="mb-4" v-show="!showLogin">Create a free account</h3>
 
-      <form class="login flex items-center justify-center flex-col gap-7 w-full" @submit="loginWithEmail"
-        @submit.prevent>
+      <form class="login flex items-center justify-center flex-col gap-7 w-full" @submit="loginWithEmail" @submit.prevent>
         <div class="relative flex items-start justify-center flex-col gap-1">
-          <label class="font-medium" for="email">Your email address <span title="Required"
-              class="text-red-500 font-2xl">*</span></label>
+          <label class="font-medium" for="email">Your email address <span title="Required" class="text-red-500 font-2xl">*</span></label>
           <input
-            class="w-96 h-12 rounded-lg border-0 bg-gray-300 px-4 transition duration-500 focus:outline focus:outline-2 focus:outline-[color:var(--primary)] focus:[color:var(--bg-fadded-color)]"
-            id="email" type="email" required v-model="email" />
+            class="w-96 h-12 rounded-lg border-0 bg-gray-300 px-4 transition duration-500 focus:outline focus:outline-2 focus:outline-[color:var(--primary)] focus:bg-[var(--bg-fadded-color)]"
+            id="email"
+            type="email"
+            required
+            v-model="email"
+          />
           <p class="absolute error font-medium text-red-500" v-show="emailErr.length > 0">{{ emailErr }}</p>
         </div>
 
         <div class="relative flex items-start justify-center flex-col gap-1" v-if="!showLogin">
-          <label class="font-medium" for="name">Your name <span title="Required"
-              class="text-red-500 font-2xl">*</span></label>
+          <label class="font-medium" for="name">Your name <span title="Required" class="text-red-500 font-2xl">*</span></label>
           <input
             class="w-96 h-12 rounded-lg border-0 bg-gray-300 px-4 transition duration-500 focus:outline focus:outline-2 focus:outline-[color:var(--primary)] focus:bg-[color:var(--bg-color)]"
-            id="name" type="text" required v-model="name" autocomplete="name" />
+            id="name"
+            type="text"
+            required
+            v-model="name"
+            autocomplete="name"
+          />
           <p class="absolute error font-medium text-red-500" v-show="nameErr.length > 0">{{ nameErr }}</p>
         </div>
 
         <div class="relative flex items-start justify-center flex-col gap-1">
-          <label class="font-medium" for="password">{{ showLogin ? "Your" : "Choose a" }} password <span
-              title="Required" class="text-red-500 font-2xl">*</span></label>
+          <label class="font-medium" for="password">{{ showLogin ? "Your" : "Choose a" }} password <span title="Required" class="text-red-500 font-2xl">*</span></label>
           <input
             class="w-96 h-12 rounded-lg border-0 bg-gray-300 px-4 transition duration-500 focus:outline focus:outline-2 focus:outline-[color:var(--primary)] focus:bg-[color:var(--bg-color)]"
-            id="password" type="password" required v-model="password"
-            :autocomplete="showLogin ? 'current-password' : 'new-password'" />
+            id="password"
+            type="password"
+            required
+            v-model="password"
+            :autocomplete="showLogin ? 'current-password' : 'new-password'"
+          />
           <p class="absolute error font-medium text-red-500" v-show="passwordErr.length > 0">{{ passwordErr }}</p>
         </div>
 
         <div class="relative flex items-start justify-center flex-col gap-1" v-if="!showLogin">
-          <label class="font-medium" for="password">Confirm password <span title="Required"
-              class="text-red-500 font-2xl">*</span></label>
+          <label class="font-medium" for="password">Confirm password <span title="Required" class="text-red-500 font-2xl">*</span></label>
           <input
             class="w-96 h-12 rounded-lg border-0 bg-gray-300 px-4 transition duration-500 focus:outline focus:outline-2 focus:outline-[color:var(--primary)] focus:bg-[color:var(--bg-color)]"
-            id="password" type="password" required v-model="confirmPassword" autocomplete="new-password" />
-          <p class="absolute error font-medium text-red-500" v-show="confirmPasswordErr.length > 0">{{
-            confirmPasswordErr }}</p>
+            id="password"
+            type="password"
+            required
+            v-model="confirmPassword"
+            autocomplete="new-password"
+          />
+          <p class="absolute error font-medium text-red-500" v-show="confirmPasswordErr.length > 0">{{ confirmPasswordErr }}</p>
         </div>
 
         <button class="du-btn du-btn-wide du-btn-md bg-green-accent" type="submit">
@@ -55,16 +70,14 @@
           </p>
           <p class="flex items-center justify-center gap-2" v-else>Loading...</p>
         </button>
-        <NuxtLink to="/reset-password" class="no-underline font-medium transition duration-500 hover:underline"
-          v-if="showLogin"> Forgot password?</NuxtLink>
+        <NuxtLink to="/reset-password" class="no-underline font-medium transition duration-500 hover:underline" v-if="showLogin"> Forgot password?</NuxtLink>
       </form>
     </div>
 
     <h3 v-show="showLogin">New to Vent Defeater?</h3>
     <h3 v-show="!showLogin">Already have an account?</h3>
     <button class="bg-transparent border-0" @click="showLogin ? router.push('?signup=1') : router.push('')">
-      <h3 class="m-0 font-medium cursor-pointer transition duration-500 hover:underline">{{ showLogin ? "Sign up now" :
-        "Log in" }}</h3>
+      <h3 class="m-0 font-medium cursor-pointer transition duration-500 hover:underline">{{ showLogin ? "Sign up now" : "Log in" }}</h3>
     </button>
   </div>
 </template>
