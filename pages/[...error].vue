6455f0cd663744cf552ab3eef63606f042eba4d2
rylan-geykhman <johnsmith102508@gmail.com>
2024-10-20 16:15:09 -0400

    404 Page (working)

--- pages/[...error].vue
+++ pages/[...error].vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
-    <h1>oopsie poopsie this page doesnt exist</h1>
+  <div class="min-h-screen flex items-center justify-center bg-[var(--bg-color)]">
+    <div class="p-6 max-w-md mx-auto bg-[var(--bg-color)] flex flex-col items-center">
+      <h1 class="text-[6rem] md:text-[10rem] font-bold text-[var(--text-color)] text-center -mb-5">404</h1>
+      <p class="text-lg text-[var(--text-color)] text-center mb-4">Sorry, we couldn't find the page you were looking for.</p>
+      <NuxtLink to="/" class="px-8 py-2 text-xl font-medium rounded-full bg-green-accent text-[var(--text-color)] hover:bg-opacity-90 transition">Back to Home</NuxtLink>
+    </div>
   </div>
 </template>
 
