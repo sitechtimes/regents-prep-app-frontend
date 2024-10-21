08e3668da52d2338bfb6b2bfed71e90939489d3f
AbigailLabanok <abigail.labanok@gmail.com>
2024-10-21 10:58:47 -0400

    Update create-class.vue

--- pages/teacher/create-class.vue
+++ pages/teacher/create-class.vue
@@ -2,7 +2,7 @@
   <div class="flex items-center justify-center flex-col w-screen min-h-screen bg-gray py-12">
     <div class="flex items-center justify-center flex-col bg-[color:var(--bg-color)] p-4 rounded-3xl mb-4">
       <h1 class="text-5xl font-bold mb-4">Create New Class</h1>
-      <form class="login flex items-center justify-center flex-col gap-7 w-full" @submit.prevent>
+      <form class="login flex items-center justify-center flex-col gap-7 w-full" @submit="createNewClass" @submit.prevent>
         <div class="relative flex items-start justify-center flex-col gap-1">
           <label class="font-medium" for="class-name">Class Name <span title="Required" class="text-red-500 font-2xl">*</span></label>
           <input
@@ -22,12 +22,12 @@
             required
             v-model="selectedOption"
           >
-            <option v-for="regents in regentsTypes" :key="regents.id" :value="regents.name">{{ regents.name }}</option>
+            <option v-for="regent in regentTypes" :key="regent.id" :value="regent.name">{{ regent.name }}</option>
           </select>
         </div>
-        <button class="du-btn du-btn-wide du-btn-md bg-green-accent" v-if="selectedOption && className" onclick="my_modal_3.showModal()">Create</button>
+        <button class="du-btn du-btn-wide du-btn-md bg-green-accent" v-if="selectedOption && className" @click="myModal3?.showModal()">Create</button>
         <button class="du-btn du-btn-wide du-btn-md bg-green-accent" v-else>Create</button>
-        <dialog id="my_modal_3" class="modal rounded-2xl w-fit h-30 p-6 outline outline-2 outline-[color:var(--primary)]">
+        <dialog ref="myModal3" class="modal rounded-2xl w-fit h-30 p-6 outline outline-2 outline-[color:var(--primary)]">
           <div class="modal-box">
             <form method="dialog">
               <button class="btn btn-sm btn-circle btn-ghost absolute rounded-full right-2 top-2 h-8 w-8 text-sm duration-300 hover:bg-[var(--gray)]" @click="pushUserBack">✕</button>
@@ -46,21 +46,25 @@ definePageMeta({
   layout: "teacher"
 });
 
+const myModal3 = ref<HTMLDialogElement>();
+
 const className = ref("");
 const selectedOption = ref("");
 const router = useRouter();
 
-interface regents {
+type Regent = {
   id: number;
   name: string;
-}
+};
 
 //placeholder array
-const regentsTypes: regents[] = [
+const regentTypes: Regent[] = [
   { id: 1, name: "Physics" },
   { id: 2, name: "Algebra" },
   { id: 3, name: "Chemistry" }
 ];
+
+function createNewClass() {}
 
 function pushUserBack() {
   router.push("/teacher/dashboard");
