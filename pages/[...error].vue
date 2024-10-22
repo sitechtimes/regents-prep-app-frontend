39e2be64f5fd3239c06b7d9a76240908f772e774
DubNubz <iamanasianboi12457@gmail.com>
2024-10-21 23:31:57 -0400

    MAD nuclear deterrence

--- pages/[...error].vue
+++ pages/[...error].vue
@@ -1,15 +1,15 @@
 <template>
   <div class="min-h-screen flex items-center justify-center bg-[var(--bg-color)]">
     <div class="p-6 max-w-md mx-auto bg-[var(--bg-color)] flex flex-col items-center">
-      <h1 class="text-8xl md:text-[10rem] mb leading-none font-extrabold text-[var(--text-color)] text-center" @click="counter--">404</h1>
-      <p class="text-lg text-[var(--text-color)] text-center mb-4">Sorry, we couldn't find the page you were looking for.</p>
+      <h1 class="text-8xl md:text-[10rem] mb leading-none font-extrabold text-[var(--text-color)] text-center select-none" @click="counter--">404</h1>
+      <p class="text-lg text-[var(--text-color)] text-center mb-4 select-none">Sorry, we couldn't find the page you were looking for.</p>
       <NuxtLink to="/" class="px-8 py-2 text-xl font-medium rounded-full bg-green-accent brightness-75 text-[var(--text-color)] hover:brightness-100 transition">Back to Home</NuxtLink>
     </div>
     <div>
       <img
         v-for="yoda in yodas"
         :key="yoda.created"
-        class="fixed select-none"
+        class="fixed select-none pointer-events-none"
         :draggable="false"
         :style="{
           left: yoda.x + 'px',
