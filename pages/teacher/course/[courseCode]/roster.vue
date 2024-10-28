52cbcc59f396d35f3b79613bb338cbf2d03a4853
AbigailLabanok <abigail.labanok@gmail.com>
2024-10-27 23:06:04 -0400

    finished up code

--- pages/teacher/course/[courseCode]/roster.vue
+++ pages/teacher/course/[courseCode]/roster.vue
@@ -1,36 +1,38 @@
 <template>
-  <div>
-    <h1>Class {{ route.params.courseCode }}</h1>
-    <div class="relative flex items-start justify-center flex-col gap-1">
-      <div>
-        <label class="input input-bordered flex items-center gap-2 min-w-[18rem] max-w-4xl">
-          <input type="text" class="grow" placeholder="Search" />
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="h-4 w-4 opacity-70">
-            <path fill-rule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clip-rule="evenodd" />
-          </svg>
-        </label>
-      </div>
-      <div class="rounded-b-box rounded-se-box relative overflow-x-auto">
-        <table class="table">
+  <div class="flex items-center justify-center flex-col w-screen min-h-screen py-12">
+    <div class="relative flex items-center justify-center flex-col gap-1">
+      <input
+        class="input input-bordered w-96 h-10 mb-6 px-2 py-1 rounded-lg border-2 transition duration-500 focus:outline focus:outline-2 focus:outline-[color:var(--primary)] focus:bg-[var(--bg-color)]"
+        type="text"
+        placeholder="Search Student..."
+        v-model="searchTerm"
+      />
+      <div class="rounded-b-box rounded-se-box relative overflow-x-auto items">
+        <table class="table flex items-center justify-center">
           <thead>
             <tr>
-              <th></th>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Remove Student</th>
+              <th class="py-3 px-10">Class {{ route.params.courseCode }}</th>
+              <th class="py-3 px-10">First Name</th>
+              <th class="py-3 px-10">Last Name</th>
+              <th class="py-3 px-10">Remove Student</th>
             </tr>
           </thead>
-          <tbody class="border-t" v-for="student in students" :key="student.id">
-            <tr>
-              <th class="py-3 px-4">{{ student.id }}</th>
-              <td class="py-3 px-4">{{ student.firstName }}</td>
-              <td class="py-3 px-4">{{ student.lastName }}</td>
-              <td class="py-3 px-4 place-content-center"><button>✕</button></td>
+          <tbody>
+            <tr class="border-t" v-for="(student, index) in filteredStudents" :key="student.id">
+              <th class="py-3 px-10">{{ student.id }}</th>
+              <td class="py-3 px-10">{{ student.firstName }}</td>
+              <td class="py-3 px-10">{{ student.lastName }}</td>
+              <td class="py-3 px-10 flex items-center justify-center">
+                <button @click="removeStudent(index)" class="btn btn-sm h-8 w-8 rounded-full transition-300 hover:bg-[var(--gray)] flex items-center justify-center">✕</button>
+              </td>
+            </tr>
+            <tr v-if="filteredStudents.length === 0" class="border-t">
+              <td colspan="4" class="text-center items-center justify-center py-3 px-10">No results found</td>
             </tr>
           </tbody>
         </table>
       </div>
-      <button @click="pushUserBack" class="du-btn du-btn-wide du-btn-md bg-green-accent">Return To Class Page</button>
+      <button @click="pushUserBack" class="du-btn du-btn-wide du-btn-md bg-green-accent mt-4">Return To Class Page</button>
     </div>
   </div>
 </template>
@@ -38,6 +40,7 @@
 <script setup lang="ts">
 const route = useRoute();
 const router = useRouter();
+const searchTerm = ref("");
 
 type Student = {
   id: number;
@@ -51,6 +54,14 @@ const students: Student[] = [
   { id: 2, firstName: "Andrea", lastName: "Guo" },
   { id: 3, firstName: "Bridget", lastName: "Feng" }
 ];
+
+const filteredStudents = computed(() => {
+  return students.filter((student) => student.firstName.toLowerCase().includes(searchTerm.value.toLowerCase()) || student.lastName.toLowerCase().includes(searchTerm.value.toLowerCase()));
+});
+
+function removeStudent(index: number): void {
+  students.splice(index, 1);
+}
 
 function pushUserBack() {
   router.push(`/teacher/course/${route.params.courseCode}`);
