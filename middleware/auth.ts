5ed6c485b9dcf139ec162ba59c4dbc9ff29549f6
rylan-geykhman <johnsmith102508@gmail.com>
2024-10-30 10:52:31 -0400

    fixing more

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,5 +1,5 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
   const userStore = useUserStore();
-  userStore.init();
+  if (!userStore.initComplete) await userStore.init();
   if (!userStore.isAuth && to.meta.requiresAuth) return navigateTo("/login");
 });
