087541743ace2046be2e90cfffebef62a76e1de6
rylan-geykhman <johnsmith102508@gmail.com>
2024-10-30 10:58:07 -0400

    removing consolelog, other stuff

--- stores/user.ts
+++ stores/user.ts
@@ -8,11 +8,8 @@ export const useUserStore = defineStore("userStore", () => {
   const currentCourse = ref<StudentCourseInfo | TeacherCourseInfo>();
   const router = useRouter();
   const initComplete = ref(false);
-  const initStarted = ref(false);
 
   async function init() {
-    if (initStarted.value) return;
-    initStarted.value = true;
     const res = await fetch(config.public.backend + "init/", {
       credentials: "include"
     });
@@ -23,7 +20,6 @@ export const useUserStore = defineStore("userStore", () => {
     userType.value = data.userType.toLowerCase();
     courses.value = data.courses;
     initComplete.value = true;
-    console.log("initCompleted:", initComplete.value);
   }
   async function login(email: string, password: string) {
     const res = await fetch(config.public.backend + "auth/login/", {
