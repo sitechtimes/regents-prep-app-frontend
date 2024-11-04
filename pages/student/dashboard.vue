5fe622f334a843542412cce0e4683c3c77a1e8eb
Batyrkhan Alimzhanov <144455676+batyrkhana@users.noreply.github.com>
2024-11-04 10:58:30 -0500

    .

--- pages/student/dashboard.vue
+++ pages/student/dashboard.vue
@@ -7,20 +7,19 @@
 
     <div class="w-full" v-else>
       <Transition name="opacity">
-        <div v-if="showNotFound" @click="router.push('/student/dashboard')" class="z-50 w-screen h-screen fixed top-0 left-0 bg-[rgba(0,0,0,0.5)] flex items-center justify-center">
-          <NotFound type="student" :message="route.query.course ? 'class' : 'assignment'" @close="router.push('/student/dashboard')" @click="$event.stopPropagation()" />
+        <div v-if="showNotFound" @click="router.push('/student/dashboard')"
+          class="z-50 w-screen h-screen fixed top-0 left-0 bg-[rgba(0,0,0,0.5)] flex items-center justify-center">
+          <NotFound type="student" :message="route.query.course ? 'class' : 'assignment'"
+            @close="router.push('/student/dashboard')" @click="$event.stopPropagation()" />
         </div>
       </Transition>
 
       <div class="h-screen flex flex-col">
         <div class="flex flex-wrap items-start justify-start align-top gap-8" v-if="courses.length > 0">
           <!--prettier-ignore-->
-          <StudentDashboardCard
-            @click="router.push(`/student/course/${course.id}`)"
-            v-for="course in (courses.filter((course) => !('joinCode' in course)) as StudentCourse[])"
-            :course="course"
-            :key="course.id"
-          />
+          <StudentDashboardCard @click="router.push(`/student/course/${course.id}`)"
+            v-for="course in (courses.filter((course) => !('joinCode' in course)) as StudentCourse[])" :course="course"
+            :key="course.id" />
         </div>
 
         <div class="flex items-center justify-center h-full w-full overflow-hidden" v-else>
@@ -28,7 +27,9 @@
             <h2 class="text-4xl md:text-5xl font-extrabold text-[var(--text-color)]">No Classes Found!</h2>
             <p class="text-lg text-[var(--text-color)]">It looks like you haven't added any classes yet.</p>
             <p class="text-md text-[var(--text-color)]">Click the button below to add your first class!</p>
-            <button @click="openTheJoin" class="mt-4 px-4 py-2 bg-green-accent text-white rounded-lg hover:bg-gray-600 transition duration-200">Add Class</button>
+            <button @click="openTheJoin"
+              class="mt-4 px-4 py-2 bg-green-accent text-white rounded-lg hover:bg-gray-600 transition duration-200">Add
+              Class</button>
           </div>
         </div>
         <StudentJoinClass :show="showJoinClass" @close="showJoinClass = false" />
