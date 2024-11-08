1923412aaf45a8a627e2413884a22a41d55970cb
Kenneth Ng <145921389+DubNubz@users.noreply.github.com>
2024-11-08 13:12:00 -0500

    add dark mode

--- pages/teacher/course/[courseCode]/index.vue
+++ pages/teacher/course/[courseCode]/index.vue
@@ -1,32 +1,27 @@
 <template>
   <div>
-    <!-- Loading screen -->
-    <div v-if="!loaded" class="fixed inset-0 bg-black bg-opacity-85 flex flex-col items-center justify-center text-white z-50">
-      <div class="flex space-x-3">
-        <div class="w-4 h-4 bg-green-500 rounded-full animate-pulse" style="animation-delay: 0s;"></div>
-        <div class="w-4 h-4 bg-green-500 rounded-full animate-pulse" style="animation-delay: 0.2s;"></div>
-        <div class="w-4 h-4 bg-green-500 rounded-full animate-pulse" style="animation-delay: 0.4s;"></div>
-      </div>
-      <p class="mt-4 text-2xl font-bold text-white opacity-90">Loading your content...</p>
-    </div>
-
-    <div v-else>
+    <div>
       <!-- Main content container -->
       <div class="w-full h-[80%] flex gap-4 p-4">
-        
         <!-- Current Assignments Section -->
-        <div class=" flex-1 flex flex-col items-center border-2 border-gray-200 rounded-lg p-4">
+        <div class="flex-1 flex flex-col items-center border-2 border-gray-200 dark:border-gray-700 rounded-lg p-4">
           <h3 class="font-bold text-3xl pb-2">Current Assignments:</h3>
-          <p class="text-center mb-4" v-if="!currentAssignments.length">No Current Assignments</p>
-          <div class="w-full flex flex-col gap-4">
-            <div v-for="assignment in currentAssignments" :key="assignment.id"
-              class="border-2 border-gray-200 rounded-lg p-2 bg-gray-100 shadow-md transition-transform duration-200 hover:scale-105 mb-2">
-              <p class="font-medium text-center">{{ assignment.name }}</p>
-              <p class="text-center">Due: {{ formatDate(assignment.dueDate, currentDate) }}</p>
-              <div class="text-center mt-2">
-                <span class="font-semibold mr-1">Class Submissions:</span>
-                <div class="border border-gray-400 rounded-full px-2 py-1 inline-block mt-1">
-                  {{ assignment.submissions }}/{{ (currentCourse as TeacherCourse).students ?? 0 }} Students
+          <div class="loading-div w-full h-28 rounded-lg border-2 border-gray-200 dark:border-gray-700 shadow-md" v-if="!loaded"></div>
+          <div class="flex flex-col items-center justify-center w-full" v-else>
+            <p class="text-center mb-4" v-if="!currentAssignments.length">No Current Assignments</p>
+            <div class="w-full flex flex-col gap-4">
+              <div
+                v-for="assignment in currentAssignments"
+                :key="assignment.id"
+                class="border-2 border-gray-200 dark:border-gray-700 rounded-lg p-2 bg-gray-100 dark:bg-gray-900 shadow-md transition duration-500 hover:scale-105 mb-2"
+              >
+                <p class="font-medium text-center">{{ assignment.name }}</p>
+                <p class="text-center">Due: {{ formatDate(assignment.dueDate, currentDate) }}</p>
+                <div class="text-center mt-2">
+                  <span class="font-semibold mr-1">Class Submissions:</span>
+                  <div class="border border-gray-400 dark:border-gray-500 rounded-full px-2 py-1 inline-block mt-1">
+                    {{ assignment.submissions }}/{{ (currentCourse as TeacherCourse).students ?? 0 }} Students
+                  </div>
                 </div>
               </div>
             </div>
@@ -34,18 +29,24 @@
         </div>
 
         <!-- Past Assignments Section -->
-        <div class="flex-1 flex flex-col items-center border-2 border-gray-200 rounded-lg p-4">
+        <div class="flex-1 flex flex-col items-center border-2 border-gray-200 dark:border-gray-700 rounded-lg p-4">
           <h3 class="font-bold text-3xl pb-2">Past Assignments:</h3>
-          <p class="text-center mb-4" v-if="!pastAssignments.length">No Past Assignments</p>
-          <div class="w-full flex flex-col gap-4">
-            <div v-for="assignment in pastAssignments" :key="assignment.id"
-              class="border-2 border-gray-200 rounded-lg p-2 bg-gray-100 shadow-md transition-transform duration-200 hover:scale-105 mb-2">
-              <p class="font-medium text-center">{{ assignment.name }}</p>
-              <p class="text-center">Due: {{ formatDate(assignment.dueDate, currentDate) }}</p>
-              <div class="text-center mt-2">
-                <span class="mr-1">Class Submissions:</span>
-                <div class="border border-gray-400 rounded-full px-2 py-1 inline-block mt-1">
-                  {{ assignment.submissions }}/{{ (currentCourse as TeacherCourse).students ?? 0 }} Students
+          <div class="loading-div w-full h-28 rounded-lg border-2 border-gray-200 dark:border-gray-900 p-2 shadow-md" v-if="!loaded"></div>
+          <div class="flex flex-col items-center justify-center w-full" v-else>
+            <p class="text-center mb-4" v-if="!pastAssignments.length">No Past Assignments</p>
+            <div class="w-full flex flex-col gap-4">
+              <div
+                v-for="assignment in pastAssignments"
+                :key="assignment.id"
+                class="border-2 border-gray-200 dark:border-gray-700 rounded-lg p-2 bg-gray-100 dark:bg-gray-900 shadow-md transition duration-500 hover:scale-105 mb-2"
+              >
+                <p class="font-medium text-center">{{ assignment.name }}</p>
+                <p class="text-center">Due: {{ formatDate(assignment.dueDate, currentDate) }}</p>
+                <div class="text-center mt-2">
+                  <span class="mr-1">Class Submissions:</span>
+                  <div class="border border-gray-400 dark:border-gray-500 rounded-full px-2 py-1 inline-block mt-1">
+                    {{ assignment.submissions }}/{{ (currentCourse as TeacherCourse).students ?? 0 }} Students
+                  </div>
                 </div>
               </div>
             </div>
@@ -55,9 +56,12 @@
 
       <!-- Action buttons -->
       <div class="w-full flex gap-4 p-4">
-        <button class="w-1/2 flex justify-center items-center bg-green-500 text-black text-2xl font-semibold px-6 py-2 rounded-full transition-transform duration-300 ease-in-out">
+        <NuxtLink
+          :to="`/teacher/course/${Number(route.params.courseCode)}/roster`"
+          class="w-1/2 flex justify-center items-center bg-green-500 text-black text-2xl font-semibold px-6 py-2 rounded-full transition-transform duration-300 ease-in-out"
+        >
           <h3>View Student List</h3>
-        </button>
+        </NuxtLink>
         <button class="w-1/2 flex justify-center items-center bg-green-500 text-black text-2xl font-semibold px-6 py-2 rounded-full transition-transform duration-300 ease-in-out">
           <h3>Assign Student Homework</h3>
         </button>
@@ -83,7 +87,10 @@ const loaded = ref(false);
 const currentAssignments = ref<TeacherAssignment[]>([]);
 const pastAssignments = ref<TeacherAssignment[]>([]);
 
-async function fetchAndSetAssignments(courseId: number) {
+let ran = false;
+async function fetchAndSetAssignments(courseId: number, redirect = false) {
+  if (ran && !redirect) return;
+  ran = true;
   loaded.value = false;
 
   try {
@@ -94,13 +101,12 @@ async function fetchAndSetAssignments(courseId: number) {
     console.error("Error fetching assignments:", error);
     await router.push(`/teacher/dashboard?course=${courseId}`);
   } finally {
-    setTimeout(() => {
-      loaded.value = true;
-    }, 250);
+    await delay(250);
+    loaded.value = true;
   }
 }
 
-userStore.$subscribe(async (mutation, state) => {
+userStore.$subscribe(async () => {
   if (!userStore.initComplete) return;
 
   const courseId = Number(route.params.courseCode);
@@ -118,3 +124,20 @@ onMounted(async () => {
   await fetchAndSetAssignments(courseId);
 });
 </script>
+
+<style scoped>
+@keyframes shift {
+  0% {
+    background-position-x: -100%;
+  }
+  100% {
+    background-position-x: 100%;
+  }
+}
+
+.loading-div {
+  background-image: linear-gradient(to right, var(--gray), var(--bg-color), var(--gray));
+  background-size: 200% 100%;
+  animation: shift 1.5s infinite linear;
+}
+</style>
