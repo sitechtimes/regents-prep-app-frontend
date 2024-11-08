6ab66932e80a00746aa1bcb774b45fb109dc1ff6
DubNubz <iamanasianboi12457@gmail.com>
2024-11-07 19:05:32 -0500

    add test server middleware

--- server/middleware/mockRoutes.ts
+++ server/middleware/mockRoutes.ts
@@ -1,7 +1,7 @@
 export default defineEventHandler((event) => {
-  /*const url = getRequestURL(event);
+  const url = getRequestURL(event);
   if (process.env.NODE_ENV === "production" && url.pathname.startsWith("/api")) {
     event.node.res.statusCode = 404;
     event.node.res.end("Not found");
-  }*/
+  }
 });
