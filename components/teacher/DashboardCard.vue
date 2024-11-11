26990c25354304a80dbb350de20d79b8a7dc0352
Batyrkhan Alimzhanov <144455676+batyrkhana@users.noreply.github.com>
2024-11-11 15:30:31 -0500

    Small changes

--- components/teacher/DashboardCard.vue
+++ components/teacher/DashboardCard.vue
@@ -1,7 +1,5 @@
 <template>
-  <div
-    class="hover:border-[var(--text-color)] hover:shadow-lg hover:dark:shadow-[var(--gray)] cursor-pointer border-2 border-[var(--faded-bg-color)] rounded-lg overflow-hidden w-[45rem] flex flex-col items-center justify-center transition-all duration-300 ease-in-out"
-  >
+  <div  class="hover:border-[var(--text-color)] hover:shadow-lg hover:dark:shadow-[var(--gray)] cursor-pointer border-2 border-[var(--faded-bg-color)] rounded-lg overflow-hidden w-[45rem] flex flex-col items-center justify-center transition-all duration-300 ease-in-out" >
     <div class="w-full h-24 flex flex-col items-center justify-end p-2 drop-shadow-md" :style="{ backgroundColor: subjectColors[course.subject] }">
       <h2 :title="course.name" class="text-2xl text-center font-semibold text-nowrap overflow-ellipsis w-full overflow-hidden">{{ course.name }}</h2>
       <p class="text-sm">Period {{ course.period }}</p>
@@ -11,12 +9,7 @@
     <div class="flex flex-col items-center justify-start w-full min-h-36 h-full p-2 bg-[var(--bg-color)]">
       <h3 class="font-bold text-xl pt-1 pb-2">Assignments</h3>
       <div class="flex flex-wrap items-start justify-around w-full h-full gap-7" v-if="course.assignments.length > 0">
-        <div
-          v-for="assignment in sortedAssignments"
-          :key="assignment.id"
-          class="flex flex-col items-center justify-center h-full min-w-[45%] px-5"
-          :class="sortedAssignments.length === 1 ? 'w-full' : ''"
-        >
+        <div v-for="assignment in sortedAssignments" :key="assignment.id"  class="flex flex-col items-center justify-center h-full min-w-[45%] px-5"  :class="sortedAssignments.length === 1 ? 'w-full' : ''" >
           <p class="font-medium text-center" :title="assignment.dueDate.toLocaleString()">Due {{ formatDate(assignment.dueDate, currentTime) }}</p>
           <div class="flex flex-col items-center justify-start w-full h-full">
             <NuxtLink
@@ -25,7 +18,10 @@
               @click.stop
               >{{ assignment.name }}</NuxtLink
             >
-            <div @click.stop="router.push(`/teacher/course/${course.id}/${assignment.id}`)" class="relative flex items-center rounded-full w-full h-full bg-[var(--gray)] overflow-hidden"></div>
+            <div @click.stop="router.push(`/teacher/course/${course.id}/${assignment.id}`)" class="relative flex items-center rounded-full w-full h-full bg-[var(--gray)] overflow-hidden">
+              <div class="absolute left-0 h-full" :style="{ width: (assignment.numSubmitted / course.students) * 100 + '%', backgroundColor: subjectColors[course.subject] }"></div>
+              <span class="w-full z-10 text-center font-mono px-2"> {{ assignment.numSubmitted }}/{{ course.students }} </span>
+            </div>
           </div>
         </div>
       </div>
