018b11929892c26e7416d1976fa08467e3f797e5
bridgetf7 <fbridget0404@gmail.com>
2024-11-17 03:21:37 -0500

    better accessibility on account settings, terminated on-click outline

--- components/AccountSettings.vue
+++ components/AccountSettings.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="du-dropdown du-dropdown-end du-dropdown-hover">
-    <img tabindex="0" role="button" class="btn m-1 h-6 w-6 dark:invert" src="/ui/user.svg" alt="Open account settings" />
-    <ul tabindex="0" class="du-dropdown-content z-[1] mt-2 w-52 rounded-lg border border-[var(--border-color)] bg-[var(--bg-color)] p-2 shadow">
+    <img tabindex="0" role="button" class="m-1 h-6 w-6 outline-none dark:invert" src="/ui/user.svg" alt="Open account settings" />
+    <ul tabindex="0" class="du-dropdown-content z-[1] w-52 rounded-lg border border-[var(--border-color)] bg-[var(--bg-color)] p-2 shadow">
       <li class="flex h-10 w-full flex-col justify-center rounded-lg pl-4 text-left transition-all duration-500 hover:bg-[var(--hover-background)]">Account Settings</li>
       <li>
         <button @click="confirmLogout" class="h-10 w-full rounded-lg pl-4 text-left transition-all duration-500 hover:bg-[#ff625aa9]">Logout</button>
