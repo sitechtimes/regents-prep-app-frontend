f29b8fd874bafd52bab2220e4e657e01b0a7fdd8
rylan-geykhman <johnsmith102508@gmail.com>
2025-01-02 11:00:56 -0500

    commit
    work on collapse

--- pages/student/course/[courseCode]/[assignmentId]/stats.vue
+++ pages/student/course/[courseCode]/[assignmentId]/stats.vue
@@ -1,39 +1,46 @@
 <template>
   <div class="flex h-full w-full flex-col items-center justify-center">
-    <h2 class="text-3xl mt-6">Assignment Statistics</h2>
+    <!--<h2 class="text-2xl mt-6">{{ currentAssignment.name }}</h2>-->
+    <h3 class="text-xl mt-6">Assignment Statistics</h3>
     <p>Grade: {{ (questionsCorrect / numOfQuestions * 100).toFixed(0)}}%</p>
+    <div class="h-4 w-24 rounded-full border-[1.5px] border-gray-300">
+        <div
+          class="h-full rounded-full bg-green-500"
+          :style="{ width: (questionsCorrect / numOfQuestions) * 100 + '%' }"
+        ></div>
+      </div>
 
     <div class="w-full max-w-4xl mt-6 mb-6">
-      <table class="table-auto w-full border-collapse border border-gray-300">
+      <table class="table-auto w-full">
         <thead>
           <tr>
-            <th class="border border-gray-300 px-4 py-2">#</th>
-            <th class="border border-gray-300 px-4 py-2">Question</th>
-            <th class="border border-gray-300 px-4 py-2">Answer Type</th>
-            <th class="border border-gray-300 px-4 py-2">Correct Answer</th>
-            <th class="border border-gray-300 px-4 py-2">Your Answer</th>
-            <th class="border border-gray-300 px-4 py-2">Results</th>
+            <th class="px-4 py-2">#</th>
+            <th class="px-4 py-2">Question</th>
+            <th class="px-4 py-2">Answer Type</th>
+            <th class="px-4 py-2">Correct Answer</th>
+            <th class="px-4 py-2">Your Answer</th>
+            <th class="px-4 py-2">Results</th>
           </tr>
         </thead>
         <tbody>
           <tr
             v-for="(questionInstance, index) in questionInstances"
             :key="questionInstance.id"
-            class="hover:bg-green-300 text-center"
+            class="hover:bg-green-accent text-center"
           >
-            <td class="border border-gray-300 px-4 py-2 text-center">
+            <td class="px-4 py-2 text-center">
               {{ index + 1 }}
             </td>
-            <td class="border border-gray-300 px-4 py-2" v-html="questionInstance.question.text"></td>
-            <td class="border border-gray-300 px-4 py-2">
+            <td class="px-4 py-2" v-html="questionInstance.question.text"></td>
+            <td class="px-4 py-2">
               {{ questionInstance.question.answerType }}
             </td>
-            <td class="border border-gray-300 px-4 py-2">
+            <td class="px-4 py-2">
               {{
                 getCorrectAnswerText(questionInstance.question.answers)
               }}
             </td>
-            <td class="border border-gray-300 px-4 py-2">
+            <td class="px-4 py-2">
               {{
                 getUserAnswerText(
                   questionInstance.userAnswers[0],
@@ -41,7 +48,7 @@
                 )
               }}
             </td>
-            <td class ="border border-gray-300 px-4 py-2">
+            <td class ="px-4 py-2">
               <span :class="getResultClass(questionInstance)">
                 {{ getResultText(questionInstance) }}
               </span>
@@ -84,7 +91,7 @@ const route = useRoute();
 const assignmentId = ref(Number(route.params.assignmentId));
 const numOfQuestions = ref(0);
 const questionInstances = ref<QuestionInstance[]>([]);
-const questionsCorrect = ref(0)
+const questionsCorrect = ref(0);
 
 onMounted(async () => {
   try {
