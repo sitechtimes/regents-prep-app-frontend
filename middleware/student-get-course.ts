ca094c3afac5b040bad5d078dedf024730333a1a
Kenneth Ng <145921389+DubNubz@users.noreply.github.com>
2025-01-13 10:56:37 -0500

    remove console logs

--- middleware/student-get-course.ts
+++ middleware/student-get-course.ts
@@ -1,7 +1,6 @@
 export default defineNuxtRouteMiddleware(async (to) => {
   if (!import.meta.client) return;
 
-  console.log(1);
   const router = useRouter();
   const userStore = useUserStore();
   const { courses, currentCourse } = storeToRefs(userStore);
@@ -10,7 +9,6 @@ export default defineNuxtRouteMiddleware(async (to) => {
   const courseCode = Number(to.params.courseCode);
 
   currentCourse.value = courses.value.find((course) => course.id === courseCode);
-  console.log(currentCourse.value);
   if (!currentCourse.value) return navigateTo(`/student/dashboard?course=${courseCode}`);
 
   currentCourse.value.assignments = (await getAssignments(Number(to.params.courseCode))) as StudentAssignment[];
