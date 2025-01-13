e4ac81988236be765bc80b2366cfcea2460412cc
Kenneth Ng <145921389+DubNubz@users.noreply.github.com>
2025-01-13 10:55:55 -0500

    Update 01.init.global.ts

--- middleware/01.init.global.ts
+++ middleware/01.init.global.ts
@@ -5,9 +5,7 @@ export default defineNuxtRouteMiddleware(async (to) => {
   if (!import.meta.client || !nuxtApp.isHydrating || !nuxtApp.payload.serverRendered) return;
   const userStore = useUserStore();
   try {
-    console.log("a");
     await userStore.init();
-    console.log("b");
     if (!userStore.isAuth && to.path !== "/login") return navigateTo("/login", { redirectCode: 301 });
     else if (userStore.isAuth && ["/login", "/"].includes(to.path)) return navigateTo(`/${userStore.userType}/dashboard`, { redirectCode: 301 });
   } catch (error) {
