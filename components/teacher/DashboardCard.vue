2f085e070d4f00834cb6e91e9a851bca62d8811f
GhTmp1A <stanbutter4@gmail.com>
2025-01-17 06:30:27 -0500

    Text size changes

--- components/teacher/DashboardCard.vue
+++ components/teacher/DashboardCard.vue
@@ -3,18 +3,18 @@
     class="flex w-[45rem] cursor-pointer flex-col items-center justify-center overflow-hidden rounded-lg border border-[var(--faded-bg-color)] transition-all duration-300 ease-in-out hover:border-[var(--primary)] hover:shadow-lg hover:dark:shadow-[var(--gray)]"
   >
     <div class="flex h-28 w-full flex-col items-center justify-end p-2 drop-shadow-md" :style="{ backgroundColor: subjectColors[course.subject] }">
-      <h2 :title="course.name" class="w-full overflow-hidden overflow-ellipsis text-nowrap text-center text-2xl font-semibold">{{ course.name }}</h2>
-      <p class="text-sm">Period {{ course.period }}</p>
+      <h2 :title="course.name" class="w-full overflow-hidden overflow-ellipsis text-nowrap text-center text-4xl font-semibold">{{ course.name }}</h2>
+      <p class="text-3xl">Period {{ course.period }}</p>
       <p>{{ course.teacher }}</p>
     </div>
     <!-- Above is the code for the top of the course card -->
     <div class="flex h-full min-h-36 w-full columns-2 bg-[var(--bg-color)] p-2">
       <div class="w-full">
-        <div class="h-16 text-center text-4xl font-bold"><h3 class="self-center pt-3 underline">Class Code</h3></div>
+        <div class="h-16 text-center text-xl font-bold"><h3 class="self-center pt-3 underline">Class Code</h3></div>
       </div>
       <div class="divider divider-dark-border"></div>
       <div class="w-full">
-        <div class="h-16 text-center text-4xl font-bold"><h3 class="self-center pt-3 underline">Students in class</h3></div>
+        <div class="h-16 text-center text-xl font-bold"><h3 class="self-center pt-3 underline">Students in class</h3></div>
       </div>
       <!--       <div class="card grid h-20 flex-grow place-items-center rounded-box bg-base-300">content</div>
       <div class="divider divider-horizontal divider-neutral">test</div>
