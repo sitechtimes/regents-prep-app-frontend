39a511411d96e79f5db0de9e4618d346afd0481b
rylan-geykhman <johnsmith102508@gmail.com>
2025-01-30 11:02:27 -0500

    commit

--- pages/teacher/course/[courseCode]/create-assignment.vue
+++ pages/teacher/course/[courseCode]/create-assignment.vue
@@ -1,6 +1,12 @@
 <template>
   <p>testing 123</p>
 </template>
-<script lang="ts"></script>
+<script lang="ts">
+const route = useRoute();
+const router = useRouter();
+const userStore = useUserStore();
+
+
+</script>
 
 <style></style>
--- pages/teacher/course/[courseCode]/index.vue
+++ pages/teacher/course/[courseCode]/index.vue
@@ -49,12 +49,12 @@
       >
         <p>View Student List</p>
       </NuxtLink>
-      <button
+      <NuxtLink
+        :to="`/teacher/course/${teacherCurrentCourse.id}/create-assignment`"
         class="flex w-1/2 items-center justify-center rounded-xl bg-[var(--primary)] px-6 py-2 text-2xl text-[var(--text-color)] transition-all duration-300 ease-in-out hover:brightness-[0.85] hover:dark:brightness-125"
-        type="button"
       >
         <p>Assign Student Homework</p>
-      </button>
+      </NuxtLink>
     </div>
   </div>
 </template>
