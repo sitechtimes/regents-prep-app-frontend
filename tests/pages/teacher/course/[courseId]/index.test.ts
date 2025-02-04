5e3cfb4bb1af962d28c2692afc1c81bfe6b2d779
GhTmp1A <stanbutter4@gmail.com>
2025-02-04 11:01:42 -0500

    Testing ViTest

--- tests/pages/teacher/course/[courseId]/index.test.ts
+++ tests/pages/teacher/course/[courseId]/index.test.ts
@@ -1,11 +1,33 @@
 import { mountSuspended } from "@nuxt/test-utils/runtime";
 import { describe, expect, test, vi } from "vitest";
+import { createPinia, setActivePinia } from "pinia";
+import { beforeEach } from "vitest";
 import Index from "~/pages/teacher/course/[courseCode]/index.vue";
 
 describe("teacherCourse", async () => {
+  beforeEach(() => {
+    setActivePinia(createPinia());
+  });
+
+  test("page should be visible", async () => {
+    const page = await mountSuspended(Index);
+    expect(page.exists()).toBe(true);
+  });
+
+  test("currentAssignmentExist", async () => {
+    const page = await mountSuspended(Index);
+    const currentAssignments = ref(page.vm.currentAssignments);
+
+    function()
+
+
+    expect(currentAssignments.value).toHaveLength(1);
+  });
+
   test("currentAssignmentsTest", async () => {
     const page = await mountSuspended(Index);
     const assignments = ref(page.vm.assignments);
+
     const currentAssignments = ref(page.vm.currentAssignments);
 
     assignments.value = [
