3e27bc77ad9daed444fa5a5a5eee0dce89890cb3
bridgetf7 <fbridget0404@gmail.com>
2025-03-25 21:05:47 -0400

    gotta reset my computer

--- components/student/Assignment/AssignmentCard.vue
+++ components/student/Assignment/AssignmentCard.vue
@@ -5,7 +5,7 @@
         ? `/student/course/${course?.id ?? assignment.assignment.course?.id}/${assignment.id}/stats`
         : `/student/course/${course?.id ?? assignment.assignment.course?.id}/${assignment.id}`
     "
-    class="flex w-full flex-col items-center justify-center gap-2 rounded-2xl border border-neutral-300 py-4 hover:border-neutral-600 hover:shadow-lg sm:flex-row sm:p-6 dark:border-neutral-600 dark:hover:border-neutral-300"
+    class="flex w-full flex-col items-center justify-center gap-2 rounded-2xl border border-neutral-300 py-4 hover:border-neutral-600/50 hover:shadow-lg sm:flex-row sm:p-6 dark:border-neutral-600 dark:hover:border-neutral-300"
   >
     <div class="flex w-5/6 flex-col items-start justify-start">
       <h3 class="w-full overflow-hidden overflow-ellipsis text-nowrap text-2xl font-semibold">{{ assignment.assignment.name }}</h3>
--- components/student/Todo/Sort.vue
+++ components/student/Todo/Sort.vue
@@ -1,12 +1,9 @@
 <template>
   <div class="relative flex flex-col items-start justify-start rounded-xl">
+    <!-- sort button -->
     <button
-      class="flex h-9 w-9 items-center justify-center rounded-full border bg-white p-0 transition duration-500 lg:w-32 lg:justify-start lg:p-2 lg:px-4 dark:bg-neutral-800"
-      :class="
-        showSorters
-          ? 'border-2 border-[#794dff] shadow-sm shadow-[#794dff38] ring-0 ring-[#794dff]'
-          : 'border-neutral-300 hover:border-neutral-400 dark:border-neutral-700 dark:hover:border-neutral-700'
-      "
+      class="flex h-9 w-9 items-center justify-center rounded-full border border-neutral-300 bg-white p-0 transition duration-500 lg:w-32 lg:justify-start lg:p-2 lg:px-4 dark:bg-neutral-800"
+      :class="showSorters ? 'border-2 hover:border-neutral-600/50' : 'border-neutral-300 hover:border-neutral-400 dark:border-neutral-700 dark:hover:border-neutral-700'"
       type="button"
       @click="showSorters = !showSorters"
     >
@@ -14,10 +11,11 @@
       <p class="hidden grow transition duration-500 lg:block">Sort</p>
     </button>
 
+    <!-- sort dropdown selection -->
     <Transition name="slide-up">
       <div
         v-show="showSorters"
-        class="absolute top-10 z-[2] flex w-44 flex-col items-start justify-center gap-1 rounded-xl border-2 border-neutral-200 bg-white p-3 shadow-lg transition duration-500 dark:border-neutral-700 dark:bg-neutral-800"
+        class="absolute top-10 z-[2] flex w-44 flex-col items-start justify-center gap-1 rounded-xl border border-neutral-300 bg-white p-3 shadow-lg transition duration-500 dark:border-neutral-700 dark:bg-neutral-800"
       >
         <button v-for="(sorter, key) in sorters" class="du-label flex w-full cursor-pointer items-center justify-start gap-2" type="button" @click="selectSorter(key)">
           <input type="checkbox" class="du-checkbox pointer-events-none bg-cover" :style="{ backgroundColor: currentSorter === key ? 'inherit' : '' }" :checked="currentSorter === key" />
