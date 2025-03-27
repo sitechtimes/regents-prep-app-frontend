db3afb0548d47eb1966db60be6b21b87a14f1ba1
katie <113192783+katiekhamarkhanova@users.noreply.github.com>
2025-03-27 11:02:23 -0400

    page

--- pages/login.vue
+++ pages/login.vue
@@ -44,7 +44,6 @@
     <p>Don't have an account?</p>
     <p class="font-medium">Ask your teacher about account creation.</p>
     <NuxtLink to="/reset-password" class="mt-3 text-neutral-600"> Forgot password? Reset here. </NuxtLink>
-    <a></a>
   </div>
 </template>
 
