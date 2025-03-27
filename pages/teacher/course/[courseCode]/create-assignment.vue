c4d08f33491dd78712d91f68b3c898610b652437
GhTmp1A <stanbutter4@gmail.com>
2025-03-27 11:03:26 -0400

    Testing date localization

--- pages/teacher/course/[courseCode]/create-assignment.vue
+++ pages/teacher/course/[courseCode]/create-assignment.vue
@@ -159,9 +159,9 @@ const { showSideMenu, loadedTopics, loadedQuestions } = storeToRefs(userStore);
 
 const currentDateISO = (() => {
   const now = new Date();
-  const month = String(now.getMonth() + 1).padStart(2, "0");
-  const day = String(now.getDate()).padStart(2, "0");
-  const year = now.getFullYear();
+  const month = (now.getMonth() + 1).toLocaleString().padStart(2, "0");
+  const day = now.getDate().toLocaleString().padStart(2, "0");
+  const year = now.getFullYear().toLocaleString();
 
   return `${year}-${month}-${day}`;
 })();
@@ -180,6 +180,8 @@ const assignmentInfo = reactive({
   timeAllotted: ref<number>(),
   attemptsAllowed: ref<number>()
 });
+
+const testDate = Number(assignmentInfo.dueDate.date).toLocaleString();
 
 const allowedToSubmit = computed(() => assignmentInfo.name && (assignmentInfo.questionIds.length || assignmentInfo.topicIds.length));
 
@@ -224,7 +226,7 @@ async function createAssignment() {
       courseID,
       assignmentInfo.questionIds.filter((question) => question.isGuaranteed).map((question) => question.questionId),
       assignmentInfo.questionIds.filter((question) => !question.isGuaranteed).map((question) => question.questionId),
-      `${assignmentInfo.dueDate.date}T${assignmentInfo.dueDate.time}`,
+      `${assignmentInfo.dueDate.date}T${assignmentInfo.dueDate.date}`,
       assignmentInfo.questionIds.length,
       assignmentInfo.lateSubmissions,
       assignmentInfo.timeAllotted ?? 0,
