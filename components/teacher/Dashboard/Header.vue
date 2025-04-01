09fe232bf5eb21be08a9c2b75084572ba270bc89
Vfrid <113192881+Vfrid@users.noreply.github.com>
2025-04-01 10:41:55 -0400

    fdsa

--- components/teacher/Dashboard/Header.vue
+++ components/teacher/Dashboard/Header.vue
@@ -12,14 +12,14 @@
     </div>
 
     <div class="flex items-center justify-center">
-      <NuxtLink to="/teacher/dashboard" class="group flex items-center justify-center gap-2">
-        <span class="text-2xl font-semibold transition duration-300 group-hover:-translate-x-0.5">Regents</span>
+      <NuxtLink to="/teacher/dashboard" class="group flex items-center lg:justify-center md:justify-center justify-end gap-2">
+        <span class="text-l lg:text-2xl md:text-xl font-semibold transition duration-300 group-hover:-translate-x-0.5">Regents</span>
         <img class="size-12 rounded-full transition duration-300 group-hover:shadow-md group-active:scale-95" src="/siths.png" aria-hidden="true" />
-        <span class="text-2xl font-semibold transition duration-300 group-hover:translate-x-0.5">Prep</span>
+        <span class="text-l lg:text-2xl md:text-xl font-semibold transition duration-300 group-hover:translate-x-0.5">Prep</span>
       </NuxtLink>
     </div>
 
-    <div class="flex items-center justify-end gap-3">
+    <div class="flex items-center justify-end gap-2">
       <TeacherDashboardCreateClass :show="showClassModal" @close="showClassModal = false" />
       <button
         v-show="route.path === '/teacher/dashboard'"
