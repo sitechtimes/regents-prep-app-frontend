5f875f038a692b4ab039a59bc8ffb2e9654140ef
gliberov <124058342+gliberov@users.noreply.github.com>
2025-04-03 11:01:17 -0400

    fixing my mistakes :(

--- pages/teacher/course/[courseCode]/index.vue
+++ pages/teacher/course/[courseCode]/index.vue
@@ -10,7 +10,7 @@
         <div class="flex items-center justify-center gap-4">
           <TeacherCourseActionButton :to="`/teacher/course/${teacherCurrentCourse.id}/roster`" img="/ui/users.svg" text="View Students" />
           <TeacherCourseActionButton :to="`/teacher/course/${teacherCurrentCourse.id}/create-assignment`" img="/ui/plus.svg" text="New Assignment" />
-          <button type="button" class="text-red-500 hover:underline" @click="confirmDeleteCourse">Delete Course 🗑️</button>
+          <TeacherAssignmentCatalogQuestionButton img="/ui/trash.svg" class="text-red-500 hover:underline" text="Delete Course" :click-function="confirmDeleteCourse" />
         </div>
       </div>
 
@@ -22,7 +22,7 @@
       <div class="mt-5 flex w-full flex-wrap items-center justify-start gap-4">
         <div v-for="assignment in filteredAssignments" :key="assignment.id" class="flex items-center gap-4">
           <TeacherAssignmentCard :course="teacherCurrentCourse" :assignment="assignment" :current-date="currentDate" />
-          <button type="button" class="text-red-500 hover:underline" @click="confirmDeleteAssignment(assignment.id)">Delete Assignment 🗑️</button>
+          <button type="button" @click="confirmDeleteAssignment(assignment.id)"><img src="/ui/trash.svg" alt="" class="h-6 w-6" /> Delete Assignment</button>
         </div>
       </div>
       <DeleteModal v-model="isModalVisible" @confirm="handleConfirm" />
