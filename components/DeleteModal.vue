6bb6bdbd31960f776a8af4a80a8d06d0c8438c5c
gliberov <124058342+gliberov@users.noreply.github.com>
2025-04-04 10:58:25 -0400

    a

--- components/DeleteModal.vue
+++ components/DeleteModal.vue
@@ -1,17 +1,17 @@
 <template>
-  <Teleport to="body">
-    <div v-if="showModal" class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+  <Transition name="join-menu-scale">
+    <div v-if="showModal" class="join-menu-bg fixed left-0 top-0 z-[51] flex min-h-screen w-screen items-center justify-center bg-[rgba(0,0,0,0.15)]">
       <div class="w-96 rounded-2xl bg-white p-6 text-center shadow-lg">
         <h2 class="mb-2 text-xl font-semibold">Confirm Deletion</h2>
         <p class="mb-4 text-gray-600">{{ step === 1 ? "Are you sure you want to delete this?" : "Are you really sure?" }}</p>
         <div class="flex justify-center gap-4">
-          <button v-if="step === 1" class="rounded bg-red-500 px-4 py-2 text-white hover:bg-red-600" type="button" @click="step++">Confirm</button>
-          <button v-else class="rounded bg-red-500 px-4 py-2 text-white hover:bg-red-600" type="button" @click="confirm">Yes, Delete</button>
-          <button class="rounded bg-gray-300 px-4 py-2 text-black hover:bg-gray-400" type="button" @click="closeModal">Cancel</button>
+          <button v-if="step === 1" class="du-btn du-btn-md bg-red-500 px-4 py-2 text-white hover:bg-red-600" type="button" @click="step++">Confirm</button>
+          <button v-else class="du-btn du-btn-md bg-red-500 text-white hover:bg-red-600" type="button" @click="confirm">Yes, Delete</button>
+          <button class="du-btn du-btn-md" type="button" @click="closeModal">Cancel</button>
         </div>
       </div>
     </div>
-  </Teleport>
+  </Transition>
 </template>
 
 <script setup lang="ts">
--- pages/teacher/course/[courseCode]/index.vue
+++ pages/teacher/course/[courseCode]/index.vue
@@ -22,7 +22,7 @@
       <div class="mt-5 flex w-full flex-wrap items-center justify-start gap-4">
         <div v-for="assignment in filteredAssignments" :key="assignment.id" class="flex items-center gap-4">
           <TeacherAssignmentCard :course="teacherCurrentCourse" :assignment="assignment" :current-date="currentDate" />
-          <TeacherAssignmentCatalogQuestionButton img="/ui/trash.svg" text="Delete Assignment" :click-function="() => confirmDeleteAssignment(assignment.id)" />
+          <TeacherAssignmentCatalogQuestionButton img="/ui/trash.svg" text="Delete" :click-function="() => confirmDeleteAssignment(assignment.id)" />
         </div>
       </div>
       <DeleteModal v-model="isModalVisible" @confirm="handleConfirm" />
