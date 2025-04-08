e2b6e508d9de903d4bd8581e049ac90df536dc67
gliberov <124058342+gliberov@users.noreply.github.com>
2025-04-08 10:24:42 -0400

    Update DeleteModal.vue

--- components/DeleteModal.vue
+++ components/DeleteModal.vue
@@ -1,6 +1,6 @@
 <template>
   <Transition name="join-menu-scale">
-    <div v-if="showModal" class="join-menu-bg fixed left-0 top-0 z-[51] flex min-h-screen w-screen items-center justify-center bg-[rgba(0,0,0,0.15)]">
+    <div v-if="showModal" class="join-menu-bg fixed left-0 top-0 z-[51] flex min-h-screen w-screen items-center justify-center bg-black/15">
       <div class="w-96 rounded-2xl bg-white p-6 text-center shadow-lg">
         <h2 class="mb-2 text-xl font-semibold">Confirm Deletion</h2>
         <p class="mb-4 text-gray-600">{{ step === 1 ? "Are you sure you want to delete this?" : "Are you really sure?" }}</p>
