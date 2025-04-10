c7dc2978f48842fe0732af32e4409c21620ff5c5
Vfrid <113192881+Vfrid@users.noreply.github.com>
2025-04-10 11:03:10 -0400

    trying to fix animation
    i broke it a long time ago when i fixed the sidebar and i just realized

--- components/student/Dashboard/Sidebar/Sidebar.vue
+++ components/student/Dashboard/Sidebar/Sidebar.vue
@@ -35,7 +35,7 @@ const { studentCourses } = storeToRefs(userStore);
 <style scoped>
 .slide-right-enter-active,
 .slide-right-leave-active {
-  transition: all 0.35s ease-in-out;
+  transition: transform 0.35s ease-in-out;
 }
 
 .slide-right-enter-from,
