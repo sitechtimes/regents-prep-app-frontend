13aa3b7c826819df57f33e68d0b02dd0abeeef0e
GhTmp1A <stanbutter4@gmail.com>
2025-04-23 11:01:24 -0400

    Fixing vitest bug

--- tests/pages/teacher/course/[courseId]/roster.test.ts
+++ tests/pages/teacher/course/[courseId]/roster.test.ts
@@ -1,4 +1,18 @@
+import { mountSuspended } from "@nuxt/test-utils/runtime";
 import { flushPromises } from "@vue/test-utils";
+import { vi } from "vitest";
 import { createPinia, setActivePinia } from "pinia";
 import { beforeEach, describe, test, expect } from "vitest";
+import { ref } from "vue";
 import Page from "~/pages/teacher/course/[courseCode]/roster.vue";
+
+describe("Teacher Roster Page", () => {
+  beforeEach(() => {
+    setActivePinia(createPinia());
+  });
+
+  test("should be visible", async () => {
+    const page = await mountSuspended(Page);
+    expect(page.exists()).toBe(true);
+  });
+});
