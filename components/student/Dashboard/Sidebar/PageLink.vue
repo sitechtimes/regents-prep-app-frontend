a7a1e2580d84b608ea9e697eb86c740b450bd62d
Vfrid <113192881+Vfrid@users.noreply.github.com>
2025-05-01 10:34:25 -0400

    fixed size of sidebar student-side
    still need to fix progress bar thing and the other issues bridget requested

--- components/student/Dashboard/Sidebar/PageLink.vue
+++ components/student/Dashboard/Sidebar/PageLink.vue
@@ -1,7 +1,7 @@
 <template>
   <NuxtLink
     :to="to"
-    class="-pl-4 flex h-12 w-full items-center justify-start gap-3 rounded-xl text-xs hover:bg-neutral-200/50 md:pl-2 md:text-lg lg:pl-4 dark:hover:bg-neutral-500/25"
+    class="-pl-4 flex h-12 w-full items-center justify-start gap-3 rounded-xl text-base hover:bg-neutral-200/50 md:pl-2 md:text-lg lg:pl-4 lg:text-xl dark:hover:bg-neutral-500/25"
     :class="{ 'bg-neutral-200 dark:bg-neutral-500/25': route.path === to }"
   >
     <img class="h-8 w-8 p-1 dark:invert" :src="imgSrc" aria-hidden="true" />
--- components/student/Dashboard/Sidebar/Sidebar.vue
+++ components/student/Dashboard/Sidebar/Sidebar.vue
@@ -3,10 +3,10 @@
     <div
       v-show="showSideMenu"
       id="side-menu"
-      class="sticky left-0 top-16 flex h-[calc(100dvh-4rem)] w-40 flex-col items-start justify-start border-r border-neutral-300 pt-4 md:w-48 lg:w-64 dark:border-neutral-600"
+      class="sticky left-0 top-16 flex h-[calc(100dvh-4rem)] w-[60%] flex-col items-start justify-start border-r border-neutral-300 pt-4 md:w-48 lg:w-64 dark:border-neutral-600"
     >
       <!-- dashboard and assignments navigation -->
-      <div class="flex w-full flex-col items-center justify-center px-1 dark:border-neutral-600">
+      <div class="flex w-full flex-col items-center justify-center px-2 dark:border-neutral-600">
         <StudentDashboardSidebarPageLink to="/student/dashboard" img-src="/ui/home.svg" text="Dashboard" />
         <StudentDashboardSidebarPageLink to="/student/todo" img-src="/ui/todo.svg" text="Assignments" />
       </div>
@@ -14,7 +14,8 @@
       <!-- dividing line -->
       <div class="my-4 h-px w-full bg-neutral-300 dark:bg-neutral-600"></div>
       <!-- courses list -->
-      <div v-if="loaded" class="flex h-full w-full flex-col items-center justify-start px-2">
+
+      <div v-if="loaded" class="flex h-full w-full flex-col items-center justify-start">
         <StudentDashboardSidebarCourseLink v-for="course in studentCourses" :key="course.id" :course="course" />
       </div>
     </div>
