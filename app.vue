e8a14a1300e89a8005baf6d4dd50c80698d56f5d
Bridget <113543003+bridgetf7@users.noreply.github.com>
2025-05-02 09:43:40 -0400

    Update app.vue

--- app.vue
+++ app.vue
@@ -22,36 +22,6 @@ onBeforeMount(() => {
   if (localStorage.getItem("theme") === "dark") isDarkMode.value = true;
 });
 
-onMounted(() => {
-  console.warn(
-    `%c⠀⠀⠀⠀⠀⠀⠀⠀⡠⠂⣉⠉⠉⠉⠉⠉⢉⣀⣐⡒⠢⢀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⡠⢊⠴⠊⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠑⠢⣍⠒⢄⡀⠀⠀⠀⠀
-⠀⠀⠀⠀⡠⢪⠔⢁⠀⠤⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠑⠢⣈⠒⠄⡀⠀
-⠀⠀⢀⢊⠞⢀⠖⠁⠀⠀⠀⡹⠀⠀⣀⠤⠔⢆⠀⠀⠀⡎⠉⠐⠒⢤⡁⠢⡈⡆
-⠀⣰⠕⠁⡔⠁⢰⠊⠉⣑⠪⠔⠂⠁⠀⠀⠀⢸⠀⠀⠀⡇⠀⠀⠀⠀⢱⠀⡇⡇
-⡌⢘⠀⢰⠀⠀⠘⡄⠘⡄⠀⠀⠀⠀⠀⡖⢈⠅⠒⠢⡀⡇⠀⢨⠑⡄⢈⠀⠇⠇
-⠃⢸⠀⠘⢄⠀⠀⠘⢄⠙⠒⠒⡄⠀⠀⣇⠎⠀⣀⡀⡇⠀⠀⢸⠀⡇⢸⠀⠀⢀
-⢠⠘⡀⠀⠀⠉⠢⢄⠀⠱⠀⠀⡇⠀⢠⣹⠀⠸⠴⠁⠇⠀⠀⠈⠚⠁⡎⠀⡇⢸
-⠸⠀⠇⠀⠀⠀⠀⠈⡆⠀⡆⠀⠇⠀⢸⢠⠀⠀⠀⡜⢰⠀⠀⡠⠤⠚⠁⠀⡇⢸
-⠀⢇⠸⠀⠁⠀⠀⠈⠀⡰⠀⢰⠀⠀⢸⠘⣄⢀⠔⠁⠸⠀⠀⡇⠀⠀⠀⢀⠇⢸
-⠀⠸⡄⢧⡑⠒⠠⠀⠊⠁⠀⡎⠀⠀⠸⠀⠀⠀⠀⠀⡄⠀⠀⢁⠀⠀⡠⠋⡔⠁
-⠀⠀⠈⠓⢌⠢⢄⠀⠀⠀⠀⠇⠀⣀⡇⠀⠀⠀⠀⠀⠓⠒⠂⠉⡠⠞⡡⠊⠀⠀
-⠀⠀⠀⠀⠀⠑⢄⠙⢆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡠⠊⢀⠞⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠙⢦⣉⣀⣉⣉⣉⣉⣉⣉⣉⡉⠉⠉⠁⠤⠔⠁⠀⠀⠀⠀⠀
-
-
-%cPlease do not paste anything into the console unless you know what you're doing!
-
-%cDO NOT UNREGENT YOUR PREP!
-
-%c*If you do know what you're doing, please apply to Advanced Computer Science Applications/Development with Mr. Whalen`,
-    "color: red;",
-    "color: blue; font-weight: bold; font-size: 16px;",
-    "color: red; font-weight: 900; font-size: 32px;",
-    "color: orange; font-weight: 200; font-size: 10px; font-family: Comic Sans MS;"
-  );
-});
-
 const userTypes: Readonly<Record<string, string>> = {
   student: "/teacher",
   teacher: "/student"
