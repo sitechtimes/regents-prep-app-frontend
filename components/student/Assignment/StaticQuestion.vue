0a8ab2aa8a6dc1f5a8cb31469bc13199fb20a826
Kenneth Ng <145921389+kennething@users.noreply.github.com>
2025-05-27 10:22:25 -0400

    407 bug add cooldown to submitting questions (#411)

--- components/student/Assignment/StaticQuestion.vue
+++ components/student/Assignment/StaticQuestion.vue
@@ -17,21 +17,25 @@
     <!-- static assignment navigation -->
     <div class="mt-8 flex w-full items-center justify-between gap-1 px-3 xs:gap-6 xs:px-10">
       <button
-        class="transition:hover relative z-40 flex items-center justify-center gap-2 rounded-xl bg-green-accent px-8 py-2 sm:px-16 dark:bg-green-600"
+        class="relative z-40 flex items-center justify-center gap-2 rounded-xl bg-green-accent px-8 py-2 hover:transition sm:px-16 dark:bg-green-600"
         type="button"
-        :disabled="currentQuestionIndex === 0"
-        :class="currentQuestionIndex === 0 ? 'cursor-not-allowed opacity-50 brightness-75 grayscale' : 'group hover:brightness-110 hover:dark:bg-green-700'"
-        @click="emit('switchQuestion', 'previous')"
+        :disabled="onCooldown || currentQuestionIndex === 0"
+        :class="onCooldown || currentQuestionIndex === 0 ? 'cursor-not-allowed opacity-50 brightness-75 grayscale' : 'group hover:brightness-110 hover:dark:bg-green-700'"
+        @click="switchQuestion('previous')"
       >
         <img class="size-5 group-hover:-translate-x-1 group-hover:transition dark:invert" src="/ui/arrow-left.svg" aria-hidden="true" />
         <span class="hidden translate-y-px text-xl xs:block">Back</span>
       </button>
       <button
-        class="transition:hover flex items-center justify-center gap-2 rounded-xl bg-green-accent px-8 py-2 sm:px-16 dark:bg-green-600"
+        class="flex items-center justify-center gap-2 rounded-xl bg-green-accent px-8 py-2 hover:transition sm:px-16 dark:bg-green-600"
         type="button"
-        :disabled="currentQuestionIndex === currentAssignment.assignment.numQuestions - 1"
-        :class="currentQuestionIndex === currentAssignment.assignment.numQuestions - 1 ? 'cursor-not-allowed opacity-50 brightness-75 grayscale' : 'group hover:brightness-110 hover:dark:bg-green-700'"
-        @click="emit('switchQuestion', 'next')"
+        :disabled="onCooldown || currentQuestionIndex === currentAssignment.assignment.numQuestions - 1"
+        :class="
+          onCooldown || currentQuestionIndex === currentAssignment.assignment.numQuestions - 1
+            ? 'cursor-not-allowed opacity-50 brightness-75 grayscale'
+            : 'group hover:brightness-110 hover:dark:bg-green-700'
+        "
+        @click="switchQuestion('next')"
       >
         <span class="hidden translate-y-px text-xl xs:block">Next</span>
         <img class="size-5 group-hover:translate-x-1 group-hover:transition dark:invert" src="/ui/arrow-right.svg" aria-hidden="true" />
@@ -58,6 +62,7 @@ const props = defineProps<{
 const emit = defineEmits<{
   changeCurrentQuestion: [StaticQuestionInterface];
   switchQuestion: ["previous" | "next"];
+  triggerAutosave: [void];
 }>();
 
 const userStore = useUserStore();
@@ -69,18 +74,32 @@ const selectedChoice = defineModel<Answer>();
 const feedbackMessage = ref("");
 const errorMessage = ref("");
 
+const onCooldown = ref(false);
+function switchQuestion(direction: "previous" | "next") {
+  onCooldown.value = true;
+  emit("switchQuestion", direction);
+  setTimeout(() => (onCooldown.value = false), 300);
+}
+
+/** in unix milliseconds */
+let lastAutosave = Date.now();
 function selectChoice(choice: Answer) {
   if (!currentQuestion.value) return;
+
   if (choice.selected) choice.selected = false;
   else {
-    currentQuestion.value?.question.answers.forEach((answer) => (answer.selected = false));
+    currentQuestion.value.question.answers.forEach((answer) => (answer.selected = false));
     choice.selected = true;
   }
+
+  if (Date.now() - lastAutosave >= 6000) {
+    lastAutosave = Date.now();
+    emit("triggerAutosave");
+  }
+
   selectedChoice.value = choice;
   (currentQuestion.value as StaticQuestionInterface).staticUserAnswer = choice.id;
-  storedStaticAnswers.value[props.currentQuestionIndex] = {
-    selectedChoice: { ...choice }
-  };
+  storedStaticAnswers.value[props.currentQuestionIndex] = { selectedChoice: { ...choice } };
 }
 
 async function getQuestionByIndex(index: number) {
